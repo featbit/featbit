4c69557110509839b81ea4b6f4d70e9aea4de346
cosmos-explorer <lian.yang.work@gmail.com>
2022-08-29 16:41:43 +0200

    init ui in progress

---
+++ modules/featbit-ui/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  url: 'https://api.featureflag.co',
+  name: 'Production'
+};
---
+++ modules/featbit-ui/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  url: 'https://api.featureflag.co',
+  name: 'Development'
+};
