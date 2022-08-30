a604adc945dcbed6a8a26b7b0388b9ea2e6cc367
deleteLater <mikcczhang@gmail.com>
2022-08-30 17:14:12 +0800

    feat(es/token-parser): add token number

---
+++ modules/evaluation-server/src/Domain/Token/TokenNumber.cs
@@ -0,0 +1,34 @@
+﻿namespace Domain.Token;
+
+public static class TokenNumber
+{
+    private static readonly Dictionary<char, char> CharacterMap = new()
+    {
+        { 'Q', '0' },
+        { 'B', '1' },
+        { 'W', '2' },
+        { 'S', '3' },
+        { 'P', '4' },
+        { 'H', '5' },
+        { 'D', '6' },
+        { 'X', '7' },
+        { 'Z', '8' },
+        { 'U', '9' },
+    };
+
+    public static ReadOnlySpan<char> Decode(ReadOnlySpan<char> characters)
+    {
+        if (characters.IsEmpty || characters.IsWhiteSpace())
+        {
+            return ReadOnlySpan<char>.Empty;
+        }
+
+        var number = new char[characters.Length];
+        for (var i = 0; i < characters.Length; i++)
+        {
+            number[i] = CharacterMap[characters[i]];
+        }
+
+        return number;
+    }
+}
---
+++ modules/evaluation-server/tests/Domain.UnitTests/Token/TokenNumberTests.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Token;
+
+namespace Domain.UnitTests.Token;
+
+public class TokenNumberTests
+{
+    [Theory]
+    [InlineData("QBWSPHDXZU", "0123456789")]
+    [InlineData("QQQ", "000")]
+    [InlineData("WPD", "246")]
+    [InlineData("BDDBXZPZPXSDQ", "1661784847360")]
+    public void Should_Decode_String(string encoded, string expected)
+    {
+        var actual = TokenNumber.Decode(encoded);
+
+        Assert.True(actual.Equals(expected, StringComparison.Ordinal));
+    }
+
+    [Theory]
+    [InlineData(null, "")]
+    [InlineData("", "")]
+    [InlineData(" ", "")]
+    public void Should_Decode_Null_Or_WhiteSpace(string encoded, string expected)
+    {
+        var actual = TokenNumber.Decode(encoded);
+
+        Assert.True(actual.Equals(expected, StringComparison.Ordinal));
+    }
+}
