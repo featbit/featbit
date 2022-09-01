d5b2baa3f3df5376cf7cf3c3b2403c0601e521ac
deleteLater <mikcczhang@gmail.com>
2022-09-01 10:26:30 +0800

    test(es/utils): test for get timestamp in ms from datetime

---
+++ modules/evaluation-server/tests/Domain.UnitTests/Utils/DateTimeExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Utils.ExtensionMethods;
+
+namespace Domain.UnitTests.Utils;
+
+public class DateTimeExtensionTests
+{
+    [Fact]
+    public void Should_Get_Timestamp_In_MillionSeconds()
+    {
+        var time = DateTime.Parse("2022-09-01 08:00:00");
+        
+        Assert.Equal(1661990400000, time.ToUnixTimeMilliseconds());
+    }
+}
