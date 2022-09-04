43593c8382cd96be11033b018dc21dd45b323f7c
cosmos-explorer <lian.yang.work@gmail.com>
2022-09-05 01:12:03 +0200

    set i18n infrastructure

--- modules/front-end/src/app/app.component.ts
+++ modules/front-end/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import packageInfo from '../../package.json';
 
+interface Locale {
+  localeCode: string;
+  label: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  locales: Locale[] = [
+    { localeCode: 'en-US', label: 'English' },
+    { localeCode: 'zh', label: '中文' },
+  ];
 
   constructor() {
     console.log(`Current Version: ${packageInfo.version}`);
---
+++ modules/front-end/src/app/locale-switcher/locale-switcher.component.ts
@@ -0,0 +1,25 @@
+import {
+  Inject,
+  Component,
+  LOCALE_ID,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-locale-switcher',
+  templateUrl: './locale-switcher.component.html',
+})
+export class LocaleSwitcherComponent {
+  locales = [
+    { code: 'en-US', name: 'English' },
+    { code: 'zh', name: '中文' },
+  ];
+
+  constructor(
+    @Inject(LOCALE_ID) public activeLocale: string
+  ) {
+  }
+
+  onLocaleChange() {
+    window.location.href = `/${this.activeLocale}`;
+  }
+}
