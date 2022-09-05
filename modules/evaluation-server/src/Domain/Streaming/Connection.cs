b87a0764d5c3e31e197c64be789f3e96713828de
deleteLater <mikcczhang@gmail.com>
2022-09-06 00:31:40 +0800

    feat(es/connection): define streaming connection

---
+++ modules/evaluation-server/src/Domain/Streaming/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.WebSockets;
+
+namespace Domain.Streaming;
+
+public class Connection
+{
+    public string Id { get; }
+
+    public WebSocket WebSocket { get; }
+
+    public int EnvId { get; }
+
+    public string Type { get; }
+
+    public string Version { get; }
+
+    public long ConnectedAt { get; }
+
+    public Connection(WebSocket webSocket, int envId, string type, string version, long connectedAt)
+    {
+        Id = Guid.NewGuid().ToString("D");
+
+        WebSocket = webSocket;
+        EnvId = envId;
+        Type = type;
+        Version = version;
+        ConnectedAt = connectedAt;
+    }
+}
---
+++ modules/evaluation-server/tests/Domain.UnitTests/Streaming/ConnectionTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.WebSockets;
+using Domain.Streaming;
+using Moq;
+using Version = Domain.Streaming.Version;
+
+namespace Domain.UnitTests.Streaming;
+
+public class ConnectionTests
+{
+    [Fact]
+    public void Should_Create_New_Connection()
+    {
+        var openedWebsocketMock = new Mock<WebSocket>();
+        openedWebsocketMock.Setup(x => x.State).Returns(WebSocketState.Open);
+
+        var connection = new Connection(openedWebsocketMock.Object, 1, ConnectionType.Client, Version.V1, 1662395291241);
+        
+        Assert.True(Guid.TryParse(connection.Id, out _));
+        Assert.Equal(WebSocketState.Open, connection.WebSocket.State);
+        Assert.Equal(1, connection.EnvId);
+        Assert.Equal(ConnectionType.Client, connection.Type);
+        Assert.Equal(Version.V1, connection.Version);
+        Assert.Equal(1662395291241, connection.ConnectedAt);
+    }
+}
