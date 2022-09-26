625dd85841d4acc5c90c2b71642475dbecbd1fc6
deleteLater <mikcczhang@gmail.com>
2022-09-26 10:06:23 +0800

    refactor(api/organization): rename GetUserOrganization to GetOrganizationList

--- modules/back-end/src/Application/Organizations/GetOrganizationList.cs
+++ modules/back-end/src/Application/Organizations/GetOrganizationList.cs
@@ -1,22 +1,22 @@
 namespace Application.Organizations;
 
-public class GetUserOrganization : IRequest<IEnumerable<OrganizationVm>>
+public class GetOrganizationList : IRequest<IEnumerable<OrganizationVm>>
 {
     public string UserId { get; set; }
 }
 
-public class GetUserOrganizationHandler : IRequestHandler<GetUserOrganization, IEnumerable<OrganizationVm>>
+public class GetOrganizationListHandler : IRequestHandler<GetOrganizationList, IEnumerable<OrganizationVm>>
 {
     private readonly IOrganizationService _organizationService;
     private readonly IMapper _mapper;
 
-    public GetUserOrganizationHandler(IOrganizationService organizationService, IMapper mapper)
+    public GetOrganizationListHandler(IOrganizationService organizationService, IMapper mapper)
     {
         _organizationService = organizationService;
         _mapper = mapper;
     }
 
-    public async Task<IEnumerable<OrganizationVm>> Handle(GetUserOrganization request, CancellationToken cancellationToken)
+    public async Task<IEnumerable<OrganizationVm>> Handle(GetOrganizationList request, CancellationToken cancellationToken)
     {
         var organizations = await _organizationService.GetUserOrganizationAsync(request.UserId);
 
