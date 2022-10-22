f40d3b7501853905e21a421d05f19350c6cb160a
deleteLater <mikcczhang@gmail.com>
2022-10-22 23:46:52 +0800

    feat(api/feature-flag): publish feature flag changed notification when toggle flag

--- modules/back-end/src/Application/FeatureFlags/ToggleFeatureFlag.cs
+++ modules/back-end/src/Application/FeatureFlags/ToggleFeatureFlag.cs
@@ -11,20 +11,23 @@ public class ToggleFeatureFlagHandler : IRequestHandler<ToggleFeatureFlag, bool>
 {
     private readonly IFeatureFlagService _service;
     private readonly ICurrentUser _currentUser;
+    private readonly IPublisher _publisher;
 
-    public ToggleFeatureFlagHandler(IFeatureFlagService service, ICurrentUser currentUser)
+    public ToggleFeatureFlagHandler(IFeatureFlagService service, ICurrentUser currentUser, IPublisher publisher)
     {
         _service = service;
         _currentUser = currentUser;
+        _publisher = publisher;
     }
 
     public async Task<bool> Handle(ToggleFeatureFlag request, CancellationToken cancellationToken)
     {
         var flag = await _service.GetAsync(request.Id);
-
         flag.Toggle(_currentUser.Id);
-
         await _service.UpdateAsync(flag);
+
+        // publish on feature flag change notification
+        await _publisher.Publish(new OnFeatureFlagChanged(flag), cancellationToken);
 
         return true;
     }
