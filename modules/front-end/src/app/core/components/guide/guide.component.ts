e3dc1e92fb42613bdad542b4f2580a2c67b0d4aa
Comiscience <hu-beau@outlook.com>
2022-10-24 16:10:14 +0800

    added demo url with envsecret in guide

--- modules/front-end/src/app/core/components/guide/guide.component.ts
+++ modules/front-end/src/app/core/components/guide/guide.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { OrganizationService } from '@services/organization.service';
+import { IProjectEnv } from '@shared/types';
 
 @Component({
   selector: 'guide-drawer',
@@ -10,6 +12,17 @@ export class GuideComponent {
   @Input()
   isVisible = true;
   @Output() guideDrawerClosed: EventEmitter<any> = new EventEmitter();
+
+  currentProjectEnv: IProjectEnv;
+  demoUrl: string;
+
+  constructor(
+    private organizationService: OrganizationService
+  ) {
+    const currentOrganizationProjectEnv = this.organizationService.getCurrentOrganizationProjectEnv();
+    this.currentProjectEnv = currentOrganizationProjectEnv.projectEnv;
+    this.demoUrl = `http://localhost:5173?envKey=${this.currentProjectEnv.envSecret}`;
+  }
 
   onClose() {
     this.guideDrawerClosed.emit();
