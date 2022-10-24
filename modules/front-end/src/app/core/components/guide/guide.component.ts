dde2162cfa61326b037b7f0ade74a7ddfdf4d949
Comiscience <68597908+cosmic-flood@users.noreply.github.com>
2022-10-24 17:24:11 +0800

    ✨ Try interacting with the demo (#81)
    * added demo docker file
    
    * added demo image in docker compose
    
    * added demo url with envsecret in guide
    
    * make code shorter
    
    * added ? for avoiding exception

--- modules/front-end/src/app/core/components/guide/guide.component.ts
+++ modules/front-end/src/app/core/components/guide/guide.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { OrganizationService } from '@services/organization.service';
+import { IProjectEnv } from '@shared/types';
 
 @Component({
   selector: 'guide-drawer',
@@ -10,6 +12,15 @@ export class GuideComponent {
   @Input()
   isVisible = true;
   @Output() guideDrawerClosed: EventEmitter<any> = new EventEmitter();
+
+  demoUrl: string;
+
+  constructor(
+    private organizationService: OrganizationService
+  ) {
+    const currentOrganizationProjectEnv = this.organizationService.getCurrentOrganizationProjectEnv();
+    this.demoUrl = `http://localhost:5173?envKey=${currentOrganizationProjectEnv?.projectEnv?.envSecret}`;
+  }
 
   onClose() {
     this.guideDrawerClosed.emit();
