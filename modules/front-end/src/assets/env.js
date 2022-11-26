fe8395ad39303b0a69e9ddcda49fd4d1c5cd086c
cosmos-explorer <88151306+cosmos-explorer@users.noreply.github.com>
2022-11-26 11:11:42 +0100

    ✨ Feat(ui): added docker compose file for prod (#116)
    * feat(infra): creating docker compose file for prod
    
    * feat: added prod docker compose file

---
+++ modules/front-end/src/assets/env.js
@@ -0,0 +1,8 @@
+(function(window) {
+  window.env = window.env || {};
+
+  // Environment variables
+  window["env"]["apiUrl"] = "";
+  window["env"]["demoUrl"] = "";
+  window["env"]["evaluationUrl"] = "";
+})(this);
--- modules/front-end/src/environments/environment.prod.ts
+++ modules/front-end/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: '__API_URL__',
-  demoUrl: '__DEMO_URL__',
-  evaluationUrl: '__EVALUATION_URL__'
+  url: window['env']['apiUrl'] || '__API_URL__',
+  demoUrl: window['env']['demoUrl'] || '__DEMO_URL__',
+  evaluationUrl: window['env']['evaluationUrl'] || '__EVALUATION_URL__'
 };
