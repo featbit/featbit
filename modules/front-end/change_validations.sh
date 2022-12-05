15659dd8b62bb022a8485485dac3de55be57dca7
cosmos-explorer <lian.yang.work@gmail.com>
2022-12-05 17:49:09 +0100

    feat(ui): added workflow to validate UI changes

---
+++ modules/front-end/change_validations.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Use this script to validate the UI changes
+# This script should be ran after running npm run i18n
+
+# Reset
+COLOR_OFF='\033[0m'       # Text Reset
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+
+
+# i18n validations
+echo "i18n validations: Validating i18n..."
+if grep -Fxq "<target></target>" ./src/locale/messages.zh.xlf
+then
+  echo -e "${RED}i18n validations: Empty translations found in src/locale/messages.zh.xlf${COLOR_OFF}"
+  exit 1
+else
+  echo -e "${GREEN}i18n validations: Passed${COLOR_OFF}"
+fi
