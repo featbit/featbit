9401c6e07c8ee5b4302f89cb8525fe8233e8c6e1
Comiscience <hu-beau@outlook.com>
2022-12-08 11:51:08 +0800

    added some k6 test scripts

---
+++ modules/benchmark/k6/get-token.js
@@ -0,0 +1,30 @@
+const envSecret = 'NF6FATso3UWtoxOlb0GQXAwNRhIdinlU-EsWZcDNQAgw';
+
+const alphabet = {
+  "0": "Q",
+  "1": "B",
+  "2": "W",
+  "3": "S",
+  "4": "P",
+  "5": "H",
+  "6": "D",
+  "7": "X",
+  "8": "Z",
+  "9": "U",
+}
+
+function encodeNumber(param, length) {
+  var s = "000000000000" + param;
+  const numberWithLeadingZeros = s.slice(s.length - length);
+  return numberWithLeadingZeros.split('').map(n => alphabet[n]).join('');
+}
+
+export function getToken() {
+  const text = envSecret.replace(/=*$/, '');
+  const timestamp = Date.now();
+  const timestampCode = encodeNumber(timestamp, timestamp.toString().length);
+  // get random number less than the length of the text as the start point, and it must be greater or equal to 2
+  const start = Math.max(Math.floor(Math.random() * text.length), 2);
+
+  return `${encodeNumber(start, 3)}${encodeNumber(timestampCode.length, 2)}${text.slice(0, start)}${timestampCode}${text.slice(start)}`;
+}
