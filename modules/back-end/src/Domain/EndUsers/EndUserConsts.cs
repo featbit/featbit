baa72b61053b4183e0e3b2d15fa4a29364818c82
deleteLater <mikcczhang@gmail.com>
2022-12-12 14:53:59 +0800

    🐛 Cannot search end-users (#155)
    * refactor(api/end-user): add KeyId and Name constants
    
    * feat(api/end-user): sort end-user by updatedAt field in descending order
    
    * fix(api/end-user): ignore built-in properties when adding custom properties filter
    
    * refactor(ui/end-user): no need to pass search properties
    
    * refactor(api/end-user): use HttpPost when searching by keyIds
    
    * refactor(api/end-user): use HttpPost when searching end-users
    
    * feat(api/end-user): support ExcludedKeyIds filter
    
    * feat(ui/end-user): exclude selected when searching target users

--- modules/back-end/src/Domain/EndUsers/EndUserConsts.cs
+++ modules/back-end/src/Domain/EndUsers/EndUserConsts.cs
@@ -2,18 +2,24 @@ namespace Domain.EndUsers;
 
 public class EndUserConsts
 {
+    public const string KeyId = "keyId";
+
+    public const string Name = "name";
+
+    public static readonly string[] BuiltInProperties = { KeyId, Name };
+
     public static IEnumerable<EndUserProperty> BuiltInUserProperties(Guid envId)
     {
         var keyId = new EndUserProperty(
             envId,
-            "keyId",
+            KeyId,
             Array.Empty<EndUserPresetValue>(),
             isBuiltIn: true, isDigestField: true, remark: "user identifier in this environment"
         );
 
         var name = new EndUserProperty(
             envId,
-            "name",
+            Name,
             Array.Empty<EndUserPresetValue>(),
             isBuiltIn: true
         );
