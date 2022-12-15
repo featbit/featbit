a95afa0c2ed65199c6710e607cf7e0bbc2b9d29d
deleteLater <mikcczhang@gmail.com>
2022-12-15 14:43:33 +0800

    feat(api/audit-log): write more feature flag audit logs
    - update setting
    - update variation
    - toggle flag
    - copy flag

--- modules/back-end/src/Application/FeatureFlags/ToggleFeatureFlag.cs
+++ modules/back-end/src/Application/FeatureFlags/ToggleFeatureFlag.cs
@@ -1,4 +1,5 @@
 using Application.Users;
+using Domain.AuditLogs;
 
 namespace Application.FeatureFlags;
 
@@ -12,19 +13,29 @@ public class ToggleFeatureFlagHandler : IRequestHandler<ToggleFeatureFlag, bool>
     private readonly IFeatureFlagService _service;
     private readonly ICurrentUser _currentUser;
     private readonly IPublisher _publisher;
+    private readonly IAuditLogService _auditLogService;
 
-    public ToggleFeatureFlagHandler(IFeatureFlagService service, ICurrentUser currentUser, IPublisher publisher)
+    public ToggleFeatureFlagHandler(
+        IFeatureFlagService service,
+        ICurrentUser currentUser,
+        IPublisher publisher,
+        IAuditLogService auditLogService)
     {
         _service = service;
         _currentUser = currentUser;
         _publisher = publisher;
+        _auditLogService = auditLogService;
     }
 
     public async Task<bool> Handle(ToggleFeatureFlag request, CancellationToken cancellationToken)
     {
         var flag = await _service.GetAsync(request.Id);
-        flag.Toggle(_currentUser.Id);
+        var dataChange = flag.Toggle(_currentUser.Id);
         await _service.UpdateAsync(flag);
+
+        // write audit log
+        var auditLog = AuditLog.ForUpdate(flag, dataChange, string.Empty, _currentUser.Id);
+        await _auditLogService.AddOneAsync(auditLog);
 
         // publish on feature flag change notification
         await _publisher.Publish(new OnFeatureFlagChanged(flag), cancellationToken);
--- modules/back-end/src/Application/FeatureFlags/UpdateSetting.cs
+++ modules/back-end/src/Application/FeatureFlags/UpdateSetting.cs
@@ -1,5 +1,6 @@
 using Application.Bases;
 using Application.Users;
+using Domain.AuditLogs;
 
 namespace Application.FeatureFlags;
 
@@ -28,19 +29,29 @@ public class UpdateSettingHandler : IRequestHandler<UpdateSetting, bool>
     private readonly IFeatureFlagService _service;
     private readonly ICurrentUser _currentUser;
     private readonly IPublisher _publisher;
+    private readonly IAuditLogService _auditLogService;
 
-    public UpdateSettingHandler(IFeatureFlagService service, ICurrentUser currentUser, IPublisher publisher)
+    public UpdateSettingHandler(
+        IFeatureFlagService service,
+        ICurrentUser currentUser,
+        IPublisher publisher,
+        IAuditLogService auditLogService)
     {
         _service = service;
         _currentUser = currentUser;
         _publisher = publisher;
+        _auditLogService = auditLogService;
     }
 
     public async Task<bool> Handle(UpdateSetting request, CancellationToken cancellationToken)
     {
         var flag = await _service.GetAsync(request.Id);
-        flag.UpdateSetting(request.Name, request.IsEnabled, request.DisabledVariationId, _currentUser.Id);
+        var dataChange = flag.UpdateSetting(request.Name, request.IsEnabled, request.DisabledVariationId, _currentUser.Id);
         await _service.UpdateAsync(flag);
+
+        // write audit log
+        var auditLog = AuditLog.ForUpdate(flag, dataChange, string.Empty, _currentUser.Id);
+        await _auditLogService.AddOneAsync(auditLog);
 
         // publish on feature flag change notification
         await _publisher.Publish(new OnFeatureFlagChanged(flag), cancellationToken);
