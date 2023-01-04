867a89927ea2e7cc8f6d9ff69b39792b90e2bc76
deleteLater <mikcczhang@gmail.com>
2023-01-04 22:34:50 +0800

    small fix

--- modules/front-end/src/app/features/safe/end-users/index/index.component.ts
+++ modules/front-end/src/app/features/safe/end-users/index/index.component.ts
@@ -8,7 +8,6 @@ import { IUserProp, IUserType } from "@shared/types";
 import { EnvUserFilter } from "@features/safe/end-users/types/featureflag-user";
 import { CURRENT_USER_FILTER_ATTRIBUTE } from "@utils/localstorage-keys";
 import { EnvUserService } from "@services/env-user.service";
-import {getPathPrefix} from "@utils/index";
 
 
 @Component({
@@ -134,11 +133,7 @@ export class IndexComponent implements OnInit {
   }
 
   navigateToUserDetail(user: IUserType) {
-    const url = this.router.serializeUrl(
-      this.router.createUrlTree([`${getPathPrefix()}users/${encodeURIComponent(user.id)}`])
-    );
-
-    window.open(url, '_blank');
+    this.router.navigateByUrl(`/users/${encodeURIComponent(user.id)}`);
   }
 
   onPropsSettingClick() {
