cf633a57ebee8705fbfa8774850a85f8f2a412fb
deleteLater <mikcczhang@gmail.com>
2023-01-15 13:03:07 +0800

    code format

--- modules/back-end/src/Domain/FeatureFlags/Fallthrough.cs
+++ modules/back-end/src/Domain/FeatureFlags/Fallthrough.cs
@@ -3,7 +3,7 @@ namespace Domain.FeatureFlags;
 public class Fallthrough
 {
     public string SplittingKey { get; set; }
-    
+
     public bool IncludedInExpt { get; set; }
 
     public ICollection<RolloutVariation> Variations { get; set; }
