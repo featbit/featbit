b4ce28a8abaa2c9f4729b93a8884d575c4602154
deleteLater <mikcczhang@gmail.com>
2023-01-17 15:25:15 +0800

    integrate Verify

---
+++ modules/evaluation-server/tests/Application.IntegrationTests/Initialization.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+
+namespace Application.IntegrationTests;
+
+public static class Initialization
+{
+    [ModuleInitializer]
+    public static void Run()
+    {
+        DerivePathInfo((_, projectDirectory, type, method) => new PathInfo(
+            directory: Path.Combine(projectDirectory, "Snapshots"),
+            typeName: type.Name,
+            methodName: method.Name)
+        );
+
+        VerifySystemJson.Enable();
+    }
+}
--- modules/evaluation-server/tests/Application.IntegrationTests/WebSockets/DataSyncTests.cs
+++ modules/evaluation-server/tests/Application.IntegrationTests/WebSockets/DataSyncTests.cs
@@ -1,12 +1,10 @@
 using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Nodes;
-using Domain.Core;
-using Domain.Protocol;
 
 namespace Application.IntegrationTests.WebSockets;
 
+[UsesVerify]
 [Collection(nameof(TestApp))]
 public class DataSyncTests
 {
@@ -32,21 +30,7 @@ public class DataSyncTests
         Assert.True(result.EndOfMessage);
         Assert.Equal(WebSocketMessageType.Text, result.MessageType);
 
-        var jNode = JsonNode.Parse(buffer.AsSpan()[..result.Count]);
-        Assert.NotNull(jNode);
-
-        var jObject = jNode.AsObject();
-        var messageTypeNode = jObject["messageType"];
-        var dataNode = jObject["data"];
-        Assert.NotNull(messageTypeNode);
-        Assert.NotNull(dataNode);
-
-        Assert.Equal("data-sync", messageTypeNode.ToString());
-
-        var data = dataNode.Deserialize<ServerSdkPayload>(ReusableJsonSerializerOptions.Web);
-        Assert.NotNull(data);
-        Assert.Equal("full", data.EventType);
-        Assert.Single(data.Segments);
-        Assert.Single(data.FeatureFlags);
+        using var response = JsonDocument.Parse(buffer.AsMemory()[..result.Count]);
+        await Verify(response);
     }
 }
