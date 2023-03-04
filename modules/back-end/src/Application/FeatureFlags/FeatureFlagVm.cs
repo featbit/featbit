891dfd5f29c2153b653da2acec06f087c1f29397
Dave <fritzdavid55@outlook.at>
2023-03-04 20:00:10 +0100

    Added description to FeatureFlag

--- modules/back-end/src/Application/FeatureFlags/FeatureFlagVm.cs
+++ modules/back-end/src/Application/FeatureFlags/FeatureFlagVm.cs
@@ -7,6 +7,8 @@ public class FeatureFlagVm
     public string Id { get; set; }
 
     public string Name { get; set; }
+    
+    public string Description { get; set; }
 
     public string Key { get; set; }
 
