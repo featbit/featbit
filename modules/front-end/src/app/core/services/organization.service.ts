af55851149040a4109d727801268460748b5b4ac
cosmos-explorer <lian.yang.work@gmail.com>
2023-03-21 15:28:48 +0100

    fixed error

--- modules/front-end/src/app/core/services/organization.service.ts
+++ modules/front-end/src/app/core/services/organization.service.ts
@@ -29,7 +29,7 @@ export class OrganizationService {
   }
 
   updateOrganization(params: any): Observable<any> {
-    const url = `${this.baseUrl}/${params.id}`;
+    const url = `${this.baseUrl}`;
     return this.http.put(url, params);
   }
 
