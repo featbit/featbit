f22361eef501563edec1330a300e32acfaa7be9a
ds <sun.dian0720@gmail.com>
2023-03-28 13:19:29 +0200

    WIP-10-docker

--- modules/data-analytics/docker-entrypoint.sh
+++ modules/data-analytics/docker-entrypoint.sh
@@ -3,8 +3,13 @@
 # Abort on any error (including if wait-for-it fails).
 set -e
 
-# Wait for the backend to be up, if we know where it is.
-./wait-for-it.sh "${CLICKHOUSE_HOST}:8123 --timeout=300 --strict" 
+if [ "$LIGHT_VERSION" == "true" ]; then
+    # Wait for the backend to be up, if we know where it is.
+    ./wait-for-it.sh "${MONGO_HOST}:27017 --timeout=300 --strict" 
+else
+    # Wait for the backend to be up, if we know where it is.
+    ./wait-for-it.sh "${CLICKHOUSE_HOST}:8123 --timeout=300 --strict" 
+fi
 
 
 # Run the main container command.
