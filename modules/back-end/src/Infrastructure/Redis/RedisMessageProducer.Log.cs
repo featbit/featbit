3639041eeaf171f98db36a0f8437a045352930f6
deleteLater <mikcczhang@gmail.com>
2023-04-02 23:12:48 +0800

    process flag & segment change message using redis pub/sub

---
+++ modules/back-end/src/Infrastructure/Redis/RedisMessageProducer.Log.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Redis;
+
+public partial class RedisMessageProducer
+{
+    public static partial class Log
+    {
+        [LoggerMessage(1, LogLevel.Debug, "Message {Message} was published successfully.", EventName = "MessagePublished")]
+        public static partial void MessagePublished(ILogger<RedisMessageProducer> logger, string message);
+
+        [LoggerMessage(2, LogLevel.Error, "Exception occured when publish message.", EventName = "ErrorPublishMessage")]
+        public static partial void ErrorPublishMessage(ILogger logger, Exception exception);
+    }
+}
---
+++ modules/back-end/src/Infrastructure/Redis/RedisMessageProducer.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+using Domain.Messages;
+using Domain.Utils;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace Infrastructure.Redis;
+
+public partial class RedisMessageProducer : IMessageProducer
+{
+    private readonly IDatabase _database;
+    private readonly ILogger<RedisMessageProducer> _logger;
+
+    public RedisMessageProducer(IConnectionMultiplexer redis, ILogger<RedisMessageProducer> logger)
+    {
+        _database = redis.GetDatabase();
+        _logger = logger;
+    }
+
+    public async Task PublishAsync<TMessage>(string topic, TMessage message) where TMessage : class
+    {
+        try
+        {
+            var jsonMessage = JsonSerializer.Serialize(message, ReusableJsonSerializerOptions.Web);
+            await _database.PublishAsync(topic, jsonMessage);
+
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                Log.MessagePublished(_logger, jsonMessage);
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.ErrorPublishMessage(_logger, ex);
+        }
+    }
+}
