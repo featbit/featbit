7d6d4a658f2baf95c2e50d2eeca796f046b5a053
cosmos-explorer <lian.yang.work@gmail.com>
2023-04-08 09:05:14 +0200

    revert changes

--- modules/back-end/src/Application/FeatureFlags/PatchFeatureFlag.cs
+++ modules/back-end/src/Application/FeatureFlags/PatchFeatureFlag.cs
@@ -1,6 +1,4 @@
-using Application.Users;
 using Microsoft.AspNetCore.JsonPatch;
-using Domain.AuditLogs;
 
 namespace Application.FeatureFlags;
 
@@ -16,14 +14,10 @@ public class PatchFeatureFlag : IRequest<PatchResult>
 public class PatchFeatureFlagHandler : IRequestHandler<PatchFeatureFlag, PatchResult>
 {
     private readonly IFeatureFlagService _service;
-    private readonly IPublisher _publisher;
     
-    public PatchFeatureFlagHandler(
-        IFeatureFlagService service,
-        IPublisher publisher)
+    public PatchFeatureFlagHandler(IFeatureFlagService service)
     {
         _service = service;
-        _publisher = publisher;
     }
 
     public async Task<PatchResult> Handle(PatchFeatureFlag request, CancellationToken cancellationToken)
@@ -37,12 +31,9 @@ public class PatchFeatureFlagHandler : IRequestHandler<PatchFeatureFlag, PatchRe
         {
             return PatchResult.Fail(error);
         }
-        
+
         await _service.UpdateAsync(flag);
-        
-        // publish on feature flag change notification
-        await _publisher.Publish(new OnFeatureFlagChanged(flag), cancellationToken);
-        
+
         return PatchResult.Ok();
     }
 }
