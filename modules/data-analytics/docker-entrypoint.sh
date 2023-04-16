994023cc7a89cbd48a71d62c6d073102cf44ebe5
s2002a <38680131+dsun0720@users.noreply.github.com>
2023-04-16 18:01:20 +0200

    feat: add CHECK_DB_LIVNESS params (#320)

--- modules/data-analytics/docker-entrypoint.sh
+++ modules/data-analytics/docker-entrypoint.sh
@@ -3,12 +3,13 @@
 # Abort on any error (including if wait-for-it fails).
 set -e
 
-if [ "$IS_PRO" = false ]; then
-    ./wait-for-it.sh "${MONGO_HOST}:27017 --timeout=300 --strict" 
-else
-    ./wait-for-it.sh "${CLICKHOUSE_HOST}:8123 --timeout=300 --strict" 
+if [ "$CHECK_DB_LIVNESS" = true ]; then
+    if [ "$IS_PRO" = false ]; then
+        ./wait-for-it.sh "${MONGO_HOST}:27017 --timeout=300 --strict" 
+    else
+        ./wait-for-it.sh "${CLICKHOUSE_HOST}:8123 --timeout=300 --strict" 
+    fi
 fi
-
 
 # Run the main container command.
 exec "$@"
