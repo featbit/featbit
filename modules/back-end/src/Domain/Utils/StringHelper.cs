973ac34af26acdcbb7c2935efff22a13431a3fe3
Exilon <explunki@outlook.com>
2023-04-28 16:56:20 +0400

    Fixed the .vs issue

---
+++ modules/back-end/src/Domain/Utils/StringHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace Domain.Utils;
+public static class StringHelper
+{
+    /// <summary>
+    /// This regular expression matches any single character that is one of the following special characters:
+    /// . * + ? ^ = ! : $ { } ( ) | [ ] / \
+    /// It can be used to search for any of these special characters in a given text or string.
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns>The replaced string</returns>
+    public static string EscapeRegex(string s)
+    {
+        return Regex.Replace(s, @"([.*+?^=!:${}()|[\]/\\])", @"\$1");
+    }
+}
+
---
+++ modules/back-end/tests/Domain.UnitTests/Utils/StringHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Domain.UnitTests.Utils
+{
+    public class StringHelperTests
+    {
+        [Theory]
+        [InlineData(@"My password is not (*)!/\?", @"My password is not \(\*\)\!\/\\\?")]
+        [InlineData("", "")]
+        [InlineData("hello.world", @"hello\.world")]
+        [InlineData("hello*world", @"hello\*world")]
+        [InlineData(@"\\\\\\\\", @"\\\\\\\\\\\\\\\\")]
+        [InlineData(@"!..{?***?/\\?*^+=:$$}", @"\!\.\.\{\?\*\*\*\?\/\\\\\?\*\^\+\=\:\$\$\}")]
+        public void TestEscapeRegex(string input, string expected)
+        {
+            string actual = StringHelper.EscapeRegex(input);
+            Assert.Equal(actual, expected);
+        }
+    }
+}
