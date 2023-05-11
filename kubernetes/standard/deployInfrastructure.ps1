4a73b9815c9e8bdd6a5970c550e33d29e44d0b65
rbrennan <128187904+wss-rbrennan@users.noreply.github.com>
2023-05-11 11:38:58 -0400

    Add Kubernetes Manifests for Featbit Standard (#354)
    * Add Health Check to UI container
    
    * Adding Kubernetes Manifests for Featbit Standard
    
    * More documentation for Featbit Standard Kubernetes Manifests
    
    * Update Readme Port Forwarding section based on comments
    
    * Change extension of ingress files to be consistent

---
+++ kubernetes/standard/deployInfrastructure.ps1
@@ -0,0 +1,30 @@
+$namespace = "featbit"
+
+
+# Check if namespace exists
+if (-not (kubectl get namespaces $namespace -o name)) {
+    Write-Host "Namespace '$namespace' does not exist. Creating..."
+
+    # Create namespace
+    kubectl create namespace $namespace
+
+    Write-Host "Namespace '$namespace' created."
+} else {
+    Write-Host "Namespace '$namespace' already exists."
+}
+
+kubectl config set-context --current --namespace=$namespace
+
+kubectl apply -f .\infrastructure\mongodb-init-configMap.yaml
+
+kubectl apply -f .\infrastructure\mongodb-persistentvolumeclaim.yaml 
+
+kubectl apply -f .\infrastructure\mongodb-service.yaml
+
+kubectl apply -f .\infrastructure\mongodb-pod.yaml  
+
+kubectl apply -f .\infrastructure\redis-persistentvolumeclaim.yaml
+
+kubectl apply -f .\infrastructure\redis-service.yaml
+
+kubectl apply -f .\infrastructure\redis-pod.yaml
