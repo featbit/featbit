70cade2a934e2aa93204fc2a3219f0156af95914
cosmos-explorer <lian.yang.work@gmail.com>
2023-05-26 10:45:06 +0200

    in progress

--- modules/front-end/src/app/core/components/relay-proxy-drawer/relay-proxy-drawer.component.ts
+++ modules/front-end/src/app/core/components/relay-proxy-drawer/relay-proxy-drawer.component.ts
@@ -1,42 +1,160 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd/message';
-import { IUserPropertyPresetValue, IUserProp } from "@shared/types";
+import { IUserPropertyPresetValue, IUserProp, IProject, IEnvironment } from "@shared/types";
 import { uuidv4 } from "@utils/index";
 import { EnvUserPropService } from "@services/env-user-prop.service";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { PermissionsService } from "@services/permissions.service";
+import { generalResourceRNPattern, permissionActions } from "@shared/policy";
+import { ProjectService } from "@services/project.service";
 
 @Component({
   selector: 'relay-proxy-drawer',
   templateUrl: './relay-proxy-drawer.component.html',
   styleUrls: ['./relay-proxy-drawer.component.less']
 })
-export class RelayProxyDrawerComponent {
+export class RelayProxyDrawerComponent implements OnInit {
 
   form: FormGroup;
 
   @Input() envId: string;
   @Output() close: EventEmitter<boolean> = new EventEmitter();
 
+  isProjectsLoading: boolean = true;
+  projects: IProject[] = [];
+
   constructor(
     private envUserPropService: EnvUserPropService,
+    private projectService: ProjectService,
     private fb: FormBuilder,
-    private message: NzMessageService
+    private message: NzMessageService,
+    public permissionsService: PermissionsService,
+    private cdr: ChangeDetectorRef
   ) {
     this.initForm();
+  }
+
+  ngOnInit() {
+    const canListProjects = this.permissionsService.isGranted(generalResourceRNPattern.project, permissionActions.ListProjects);
+    if (canListProjects) {
+      this.projectService
+        .getList()
+        .subscribe({
+          next: (projects) => this.projects = projects,
+          error: (_) => this.message.error($localize`:@@common.error-occurred-try-again:Error occurred, please try again`),
+          complete: () => this.isProjectsLoading = false
+        });
+    }
   }
 
   initForm() {
     this.form = this.fb.group({
       name: [null, [Validators.required]],
       description: [null,Validators.maxLength(512)],
+      scopes: this.fb.array([]),
+      agents: this.fb.array([])
     });
+
+    //this.addAgent();
+  }
+
+  get agents(): FormArray {
+    return this.form.controls["agents"] as FormArray;
+  }
+
+  addAgent() {
+    const agentForm = this.fb.group({
+      host: ['', Validators.required]
+    });
+
+    this.agents.push(agentForm);
+    // @ts-ignore
+    // This line is necessary to refresh the table when new agent added or removed
+    this.form.controls["agents"].controls = [...this.form.controls["agents"].controls];
+  }
+
+  removeAgent(index: number) {
+    this.agents.removeAt(index);
+    // @ts-ignore
+    // This line is necessary to refresh the table when new agent added or removed
+    this.form.controls["agents"].controls = [...this.form.controls["agents"].controls];
+  }
+
+  get scopes(): FormArray {
+    return this.form.controls["scopes"] as FormArray;
+  }
+
+  addScope() {
+    const scopeForm = this.fb.group({
+      projectId: ['', Validators.required],
+      envIds: [[], Validators.required]
+    });
+
+    this.scopes.push(scopeForm);
+  }
+
+  removeLesson(index: number) {
+    this.scopes.removeAt(index);
+  }
+
+  get environments() {
+    return;
+  }
+
+  getProjectEnvs(idx: number): IEnvironment[] {
+    const { projectId } = this.scopes.at(idx).value;
+    return this.projects.find(x => x.id === projectId)?.environments;
   }
 
   doSubmit() {
+    console.log('submit');
+    let invalid = false;
+    if (this.form.invalid) {
+      for (const i in this.form.controls) {
+        this.form.controls[i].markAsDirty();
+        this.form.controls[i].updateValueAndValidity();
+      }
+      invalid = true;
+    }
 
+    // validate scopes
+    if (this.scopes.invalid) {
+      for (let control of this.scopes.controls) {
+        const scopeForm: FormGroup = control as FormGroup
+        for (const i in scopeForm.controls) {
+          scopeForm.controls[i].markAsDirty();
+          scopeForm.controls[i].updateValueAndValidity();
+        }
+
+      }
+      invalid = true;
+    }
+
+
+    // validate agents
+    if (this.agents.invalid) {
+      for (let control of this.agents.controls) {
+        const agentForm: FormGroup = control as FormGroup
+        for (const i in agentForm.controls) {
+          agentForm.controls[i].markAsDirty();
+          agentForm.controls[i].updateValueAndValidity();
+        }
+
+      }
+      invalid = true;
+    }
+
+    if (invalid) {
+      return;
+    }
+
+    const { name, description, scopes, agents } = this.form.value;
+    console.log(scopes);
+    console.log(agents);
   }
 
   private _visible: boolean = false;
+
   @Input()
   set visible(visible: boolean) {
     if (visible) {
