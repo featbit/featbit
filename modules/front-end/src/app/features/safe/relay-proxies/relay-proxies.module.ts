71339c6818313cbf1ea713af9681c29e7960a4d8
cosmos-explorer <lian.yang.work@gmail.com>
2023-05-28 22:29:32 +0200

    clean

--- modules/front-end/src/app/features/safe/relay-proxies/relay-proxies.module.ts
+++ modules/front-end/src/app/features/safe/relay-proxies/relay-proxies.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { RelayProxiesRoutingModule } from './relay-proxies-routing.module';
 import { RelayProxiesComponent } from './relay-proxies.component';
-import { DetailsComponent } from './details/details.component';
 import { IndexComponent } from './index/index.component';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzButtonModule } from 'ng-zorro-antd/button';
@@ -13,15 +12,15 @@ import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzGridModule } from "ng-zorro-antd/grid";
-import {NzSelectModule} from "ng-zorro-antd/select";
-import {NzPopoverModule} from "ng-zorro-antd/popover";
-import {NzToolTipModule} from "ng-zorro-antd/tooltip";
+import { NzSelectModule } from "ng-zorro-antd/select";
+import { NzPopoverModule } from "ng-zorro-antd/popover";
+import { NzToolTipModule} from "ng-zorro-antd/tooltip";
 import { NzDividerModule } from "ng-zorro-antd/divider";
-import {CoreModule} from "@core/core.module";
+import { CoreModule } from "@core/core.module";
 import { NzPopconfirmModule } from "ng-zorro-antd/popconfirm";
 
 @NgModule({
-  declarations: [RelayProxiesComponent, DetailsComponent, IndexComponent],
+  declarations: [RelayProxiesComponent, IndexComponent],
     imports: [
         CommonModule,
         FormsModule,
