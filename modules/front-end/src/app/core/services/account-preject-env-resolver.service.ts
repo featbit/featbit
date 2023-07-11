310387e12eaea3219a14d9e261196ddd929fedfe
cosmos-explorer <88151306+cosmos-explorer@users.noreply.github.com>
2023-07-11 07:31:33 +0200

    🧹 refactor: replace class-based resolvers with functional resolvers (#441)
    * debt: code clean
    
    * Update account-preject-env-resolver.service.ts
    
    * replace resolve by ResolveFn
    
    * replaced Resolve by ResolveFn 2
    
    * clean
    
    * refactor
    
    * more clean
    
    * rename
    
    ---------
    
    Co-authored-by: deleteLater <mikcczhang@gmail.com>

--- modules/front-end/src/app/core/services/account-preject-env-resolver.service.ts
+++ modules/front-end/src/app/core/services/account-preject-env-resolver.service.ts
@@ -1,37 +1,26 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { take, mergeMap } from 'rxjs/operators';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 import { ProjectService } from '@services/project.service';
 import { OrganizationService } from '@services/organization.service';
 import { IOrganization } from '@shared/types';
 import { IdentityService } from "@services/identity.service";
 
-@Injectable()
-export class AccountProjectEnvResolver implements Resolve<any> {
-  constructor(
-    private identityService: IdentityService,
-    private projectService: ProjectService,
-    private accountService: OrganizationService,
-  ) { }
+export const AccountProjectEnvResolver: ResolveFn<any> = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot,
+  identityService: IdentityService = inject(IdentityService),
+  projectService: ProjectService = inject(ProjectService),
+  organizationService: OrganizationService = inject(OrganizationService)
+): Observable<any> => organizationService.getCurrentOrganization().pipe(
+  mergeMap((organization: IOrganization) => {
+      if (!organization) {
+        identityService.doLogoutUser(false);
+        return;
+      }
 
-  resolve(route: ActivatedRouteSnapshot): Observable<any> {
-    return this.accountService.getCurrentOrganization().pipe(
-      take(1),
-      mergeMap((account: IOrganization) => {
-          if (!account) {
-            this.identityService.doLogoutUser(false);
-            return;
-          }
-
-          return this.projectService.setCurrentProjectEnv().pipe(
-            take(1),
-            mergeMap(() => {
-              return of({});
-            })
-          )
-        }
-      )
-    );
-  }
-}
+      return projectService.setCurrentProjectEnv();
+    }
+  )
+);
--- modules/front-end/src/app/features/safe/end-users/details/details.resolver.ts
+++ modules/front-end/src/app/features/safe/end-users/details/details.resolver.ts
@@ -1,18 +1,14 @@
-import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { EnvUserService } from "@services/env-user.service";
 import { IUserType } from "@shared/types";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class DetailsResolver implements Resolve<IUserType> {
-
-  constructor(private userService: EnvUserService) { }
-
-  resolve(route: ActivatedRouteSnapshot): Observable<IUserType> {
-    const id: string = encodeURIComponent(route.params['id']);
-    return this.userService.get(id);
-  }
-}
+export const DetailsResolver: ResolveFn<IUserType> = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot,
+  userService: EnvUserService = inject(EnvUserService)
+): Observable<IUserType> => {
+  const id: string = encodeURIComponent(route.params['id']);
+  return userService.get(id);
+};
