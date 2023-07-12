9b3ca27b6b063079dcf4c99c3093f805563de4df
deleteLater <mikcczhang@gmail.com>
2023-07-12 23:19:53 +0800

    add serilog

--- modules/back-end/src/Api/Program.cs
+++ modules/back-end/src/Api/Program.cs
@@ -1,10 +1,28 @@
 using Api.Setup;
+using Serilog;
 
-WebApplication.CreateBuilder(args)
-    .RegisterServices()
-    .Build()
-    .SetupMiddleware()
-    .Run();
+try
+{
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .CreateLogger();
+
+    WebApplication.CreateBuilder(args)
+        .ConfigureHost()
+        .RegisterServices()
+        .Build()
+        .SetupMiddleware()
+        .Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
 // https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-6.0#basic-tests-with-the-default-webapplicationfactory
 // Make the implicit Program class public so test projects can access it
