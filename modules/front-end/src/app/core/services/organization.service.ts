41a57d39dddf2340f5581a9630d6db65b52b1d67
Mücahit Çoban <80916997+mucoban@users.noreply.github.com>
2023-07-14 19:24:11 +0300

    🐛 fix: organization name doesn't update after another user update it … (#432)
    * 🐛 fix: organization name doesn't update after another user update it (#331)
    
    * 🐛 fix: take the specific organization from api which matched data from local storage(#331)

--- modules/front-end/src/app/core/services/organization.service.ts
+++ modules/front-end/src/app/core/services/organization.service.ts
@@ -67,21 +67,13 @@ export class OrganizationService {
 
   getCurrentOrganization(): Observable<IOrganization> {
     return new Observable(observer => {
-      const orgStr = localStorage.getItem(CURRENT_ORGANIZATION());
-      if (this.organizations.length === 0 || !orgStr || JSON.parse(orgStr)?.plan === undefined) {
         this.getList().subscribe(res => {
-          this.organizations = res as IOrganization[];
-          if (!orgStr) {
-            const currentOrg = this.organizations[0];
+            const orgStr = localStorage.getItem(CURRENT_ORGANIZATION());
+            this.organizations = res as IOrganization[];
+            const currentOrg = !orgStr ? this.organizations[0] : this.organizations.find(ws => ws.id === JSON.parse(orgStr).id);
             localStorage.setItem(CURRENT_ORGANIZATION(), JSON.stringify(currentOrg));
             observer.next(currentOrg);
-          } else {
-            observer.next(this.organizations.find(ws => ws.id == JSON.parse(orgStr).id));
-          }
         });
-      } else {
-        observer.next(this.organizations.find(ws => ws.id == JSON.parse(orgStr).id));
-      }
     });
   }
 }
