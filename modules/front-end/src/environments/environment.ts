fd936b720582faf444a8b110739b8485fc0f2b50
deleteLater <mikcczhang@gmail.com>
2023-07-20 15:32:52 +0800

    Revert "nit: needn't set url explicitly"
    This reverts commit cf06113bd8f6b0025b1c6f6dbc189fbe6c25df28.

--- modules/front-end/src/environments/environment.ts
+++ modules/front-end/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  url: window['env']['apiUrl'],
+  url: window['env']['apiUrl'] || location.origin.replace(/\/$/, ''),
   demoUrl: window['env']['demoUrl'] || 'https://featbit-samples.vercel.app',
-  evaluationUrl: window['env']['evaluationUrl']
+  evaluationUrl: window['env']['evaluationUrl'] || location.origin.replace(/\/$/, '')
 };
