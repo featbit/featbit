f71b8df9454a67dc70407f783774471fc99cf635
deleteLater <mikcczhang@gmail.com>
2023-08-07 10:25:52 +0800

    use tini

--- docker/ui-api-bundle/docker-entrypoint.sh
+++ docker/ui-api-bundle/docker-entrypoint.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
 set -e
 
 envsubst < /app/frontend/en/assets/env.template.js > /app/frontend/en/assets/env.js
 envsubst < /app/frontend/zh/assets/env.template.js > /app/frontend/zh/assets/env.js
 
-# Reference: https://docs.docker.com/config/containers/multi-service_container/#use-a-wrapper-script
-
 # Start Nginx
 nginx -g 'daemon off;' & 
 
 # Start Api
-./backend/Api &
+./app/backend/Api &
 
 # Wait for any process to exit
 wait -n
