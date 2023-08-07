d9df827aad7a057b06318f95c8f9adac0fffcabe
deleteLater <mikcczhang@gmail.com>
2023-08-07 09:09:04 +0800

    use bash

--- docker/ui-api-bundle/docker-entrypoint.sh
+++ docker/ui-api-bundle/docker-entrypoint.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 envsubst < /app/frontend/en/assets/env.template.js > /app/frontend/en/assets/env.js
 envsubst < /app/frontend/zh/assets/env.template.js > /app/frontend/zh/assets/env.js
+
+# Reference: https://docs.docker.com/config/containers/multi-service_container/#use-a-wrapper-script
 
 # Start Nginx
 nginx -g 'daemon off;' & 
@@ -12,7 +14,7 @@ nginx -g 'daemon off;' &
 ./backend/Api &
 
 # Wait for any process to exit
-wait
+wait -n
 
 # Exit with status of process that exited first
 exit $?
