1ee382ed0abfcf6dcf2e7d0545f25b99db56e5a7
cosmos-explorer <lian.yang.work@gmail.com>
2023-08-24 16:21:40 +0200

    feat: implement segment comparer

---
+++ modules/back-end/src/Domain/SemanticPatch/Instruction.Segment.Rule.cs
@@ -0,0 +1,80 @@
+﻿using Domain.Segments;
+using Domain.Targeting;
+
+namespace Domain.SemanticPatch;
+
+public class SetSegmentRulesInstruction : SegmentInstruction
+{
+    public SetSegmentRulesInstruction(ICollection<MatchRule> value) : base(SegmentInstructionKind.SetRules, value)
+    {
+    }
+
+    public override void Apply(Segment segment)
+    {
+        if (Value is not ICollection<MatchRule> rules)
+        {
+            return;
+        }
+
+        segment.Rules = rules;
+    }
+}
+
+public class AddSegmentRuleInstruction : SegmentInstruction
+{
+    public AddSegmentRuleInstruction(MatchRule value) : base(SegmentInstructionKind.AddRule, value)
+    {
+    }
+
+    public override void Apply(Segment segment)
+    {
+        if (Value is not MatchRule rule)
+        {
+            return;
+        }
+
+        segment.Rules.Add(rule);
+    }
+}
+
+public class RemoveSegmentRuleInstruction : SegmentInstruction
+{
+    public RemoveSegmentRuleInstruction(string value) : base(SegmentInstructionKind.RemoveRule, value)
+    {
+    }
+
+    public override void Apply(Segment segment)
+    {
+        if (Value is not string ruleId)
+        {
+            return;
+        }
+
+        var ruleToRemove = segment.Rules.FirstOrDefault(r => r.Id == ruleId);
+        if (ruleToRemove != null)
+        {
+            segment.Rules.Remove(ruleToRemove);
+        }
+    }
+}
+
+public class SegmentRuleNameInstruction : SegmentInstruction
+{
+    public SegmentRuleNameInstruction(RuleName value) : base(SegmentInstructionKind.UpdateRuleName, value)
+    {
+    }
+
+    public override void Apply(Segment segment)
+    {
+        if (Value is not RuleName value)
+        {
+            return;
+        }
+
+        var rule = segment.Rules.FirstOrDefault(r => r.Id == value.RuleId);
+        if (rule != null)
+        {
+            rule.Name = value.Name;
+        }
+    }
+}
--- modules/back-end/src/Domain/SemanticPatch/SegmentInstructionKind.cs
+++ modules/back-end/src/Domain/SemanticPatch/SegmentInstructionKind.cs
@@ -1,41 +1,15 @@
 ﻿namespace Domain.SemanticPatch;
 
-public static class FlagInstructionKind
+public static class SegmentInstructionKind
 {
-    public const string TurnFlagOn = nameof(TurnFlagOn);
-
-    public const string TurnFlagOff = nameof(TurnFlagOff);
-
+    public const string Archive = nameof(Archive);
+    
+    public const string Restore = nameof(Restore);
+    
     public const string UpdateName = nameof(UpdateName);
 
     public const string UpdateDescription = nameof(UpdateDescription);
-
-    public const string AddTags = nameof(AddTags);
-
-    public const string RemoveTags = nameof(RemoveTags);
-
-    public const string ArchiveFlag = nameof(ArchiveFlag);
-
-    public const string RestoreFlag = nameof(RestoreFlag);
-
-    public const string AddVariation = nameof(AddVariation);
-
-    public const string RemoveVariation = nameof(RemoveVariation);
-
-    public const string UpdateVariationType = nameof(UpdateVariationType);
-
-    public const string UpdateVariation = nameof(UpdateVariation);
-
-    public const string UpdateDisabledVariation = nameof(UpdateDisabledVariation);
-
-    public const string UpdateDefaultVariation = nameof(UpdateDefaultVariation);
-
-    public const string AddTargetUsers = nameof(AddTargetUsers);
-
-    public const string RemoveTargetUsers = nameof(RemoveTargetUsers);
-
-    public const string SetTargetUsers = nameof(SetTargetUsers);
-
+    
     public const string AddRule = nameof(AddRule);
 
     public const string RemoveRule = nameof(RemoveRule);
@@ -43,8 +17,6 @@ public static class FlagInstructionKind
     public const string SetRules = nameof(SetRules);
 
     public const string UpdateRuleName = nameof(UpdateRuleName);
-
-    public const string UpdateRuleDispatchKey = nameof(UpdateRuleDispatchKey);
 
     public const string AddRuleConditions = nameof(AddRuleConditions);
 
@@ -55,47 +27,32 @@ public static class FlagInstructionKind
     public const string AddValuesToRuleCondition = nameof(AddValuesToRuleCondition);
 
     public const string RemoveValuesFromRuleCondition = nameof(RemoveValuesFromRuleCondition);
-
-    public const string UpdateRuleVariationOrRollouts = nameof(UpdateRuleVariationOrRollouts);
     
-    // Segment specific instructions
     public const string AddTargetUsersToIncluded = nameof(AddTargetUsersToIncluded);
+    
     public const string RemoveTargetUsersFromIncluded = nameof(RemoveTargetUsersFromIncluded);
+    
     public const string AddTargetUsersToExcluded = nameof(AddTargetUsersToExcluded);
+    
     public const string RemoveTargetUsersFromExcluded = nameof(RemoveTargetUsersFromExcluded);
 
     public const string Noop = nameof(Noop);
 
     public static readonly string[] All =
     {
-        TurnFlagOn,
-        TurnFlagOff,
+        Archive,
+        Restore,
         UpdateName,
         UpdateDescription,
-        AddTags,
-        RemoveTags,
-        ArchiveFlag,
-        RestoreFlag,
-        AddVariation,
-        RemoveVariation,
-        UpdateVariationType,
-        UpdateVariation,
-        UpdateDisabledVariation,
-        UpdateDefaultVariation,
-        AddTargetUsers,
-        RemoveTargetUsers,
-        SetTargetUsers,
         AddRule,
         RemoveRule,
         SetRules,
         UpdateRuleName,
-        UpdateRuleDispatchKey,
         AddRuleConditions,
         RemoveRuleConditions,
         UpdateRuleCondition,
         AddValuesToRuleCondition,
         RemoveValuesFromRuleCondition,
-        UpdateRuleVariationOrRollouts,
         AddTargetUsersToIncluded,
         RemoveTargetUsersFromIncluded,
         AddTargetUsersToExcluded,
