d2da71ecf47d7f8a140465c909995aafc07f42e0
cosmos-explorer <lian.yang.work@gmail.com>
2023-10-28 13:33:25 +0200

    feat: support asterisk in license feature list

--- modules/back-end/src/Domain/Organizations/LicenseFeatures.cs
+++ modules/back-end/src/Domain/Organizations/LicenseFeatures.cs
@@ -2,11 +2,12 @@ namespace Domain.Organizations;
 
 public static class LicenseFeatures
 {
+    public const string Asterisk = "*"; // all features
     public const string Sso = "sso";
     public const string Schedule = "schedule";
     public const string CreateOrg = "create-org";
 
-    public static readonly string[] All = { Sso, Schedule, CreateOrg };
+    public static readonly string[] All = { Asterisk, Sso, Schedule, CreateOrg };
 
     public static bool IsDefined(string feature)
     {
