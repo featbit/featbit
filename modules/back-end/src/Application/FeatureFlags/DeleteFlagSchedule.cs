bdad8e5b1dbe9341c128b0715da22802153d8571
deleteLater <mikcczhang@gmail.com>
2023-11-01 00:35:54 +0800

    refactor delete schedule

--- modules/back-end/src/Application/FeatureFlags/DeleteFlagSchedule.cs
+++ modules/back-end/src/Application/FeatureFlags/DeleteFlagSchedule.cs
@@ -1,62 +1,24 @@
-using Application.Bases;
-using Application.Bases.Exceptions;
-using Domain.Organizations;
-
 namespace Application.FeatureFlags;
 
 public class DeleteFlagSchedule : IRequest<bool>
 {
     public Guid OrgId { get; set; }
+
     public Guid Id { get; set; }
 }
 
 public class DeleteFlagScheduleHandler : IRequestHandler<DeleteFlagSchedule, bool>
 {
     private readonly IFlagScheduleService _service;
-    private readonly ILicenseService _licenseService;
-    private readonly IFlagChangeRequestService _flagChangeRequestService;
 
-    public DeleteFlagScheduleHandler(
-        IFlagScheduleService service,
-        ILicenseService licenseService,
-        IFlagChangeRequestService flagChangeRequestService)
+    public DeleteFlagScheduleHandler(IFlagScheduleService service)
     {
         _service = service;
-        _licenseService = licenseService;
-        _flagChangeRequestService = flagChangeRequestService;
     }
 
     public async Task<bool> Handle(DeleteFlagSchedule request, CancellationToken cancellationToken)
     {
-        var schedule = await _service.GetAsync(request.Id);
-        if (schedule == null)
-        {
-            return true;
-        }
-
-        if (schedule.ChangeRequestId.HasValue)
-        {
-            var isChangeRequestGranted =
-                await _licenseService.IsFeatureGrantedAsync(request.OrgId, LicenseFeatures.ChangeRequest);
-
-            if (!isChangeRequestGranted)
-            {
-                throw new BusinessException(ErrorCodes.Unauthorized);
-            }
-        }
-
-        await _service.DeleteAsync(schedule.Id);
-
-        if (!schedule.ChangeRequestId.HasValue)
-        {
-            return true;
-        }
-        
-        var changeRequest = await _flagChangeRequestService.GetAsync(schedule.ChangeRequestId.Value);
-        if (changeRequest != null)
-        {
-            await _flagChangeRequestService.DeleteAsync(changeRequest.Id);
-        }
+        await _service.DeleteAsync(request.Id);
 
         return true;
     }
