b47111dd8807c07a48637116884cc735a65cfc8b
deleteLater <mikcczhang@gmail.com>
2023-12-26 00:10:14 +0800

    enhancement

--- modules/front-end/src/app/core/components/webhook-delivery/webhook-delivery.component.ts
+++ modules/front-end/src/app/core/components/webhook-delivery/webhook-delivery.component.ts
@@ -32,15 +32,23 @@ export class WebhookDeliveryComponent {
         'Content-Type: application/json',
         Object.keys(request.headers).map(x => `${x}: ${request.headers[x]}`).join('\n')
       ].join('\n');
-      this.requestPayload = JSON.stringify(JSON.parse(request.payload), null, 2);
+      this.requestPayload = this.tryFormatStringAsJson(request.payload);
     }
 
     let response = delivery.response;
     if (response) {
       this.responseHeaders = Object.keys(response.headers).map(x => `${x}: ${response.headers[x]}`).join('\n');
-      this.responseBody = response.body;
+      this.responseBody = this.tryFormatStringAsJson(response.body);
     }
 
     this.completedIn = (new Date(delivery.endedAt).getTime() - new Date(delivery.startedAt).getTime()) / 1000;
+  }
+
+  private tryFormatStringAsJson(str: string): string {
+    try {
+      return JSON.stringify(JSON.parse(str), null, 2);
+    } catch {
+      return str;
+    }
   }
 }
