ff9a9acd8cc865ff2c805064957f38dc0d4059f9
rbrennan <128187904+wss-rbrennan@users.noreply.github.com>
2024-02-16 03:03:48 -0500

    fix date formatting in experimentation to conform with date-fns (#592)

--- modules/front-end/src/app/features/safe/feature-flags/details/experimentation/experimentation.component.ts
+++ modules/front-end/src/app/features/safe/feature-flags/details/experimentation/experimentation.component.ts
@@ -118,7 +118,7 @@ export class ExperimentationComponent implements OnInit, OnDestroy {
                 expt.selectedIteration = this.processIteration({ ...expt.selectedIteration }, expt.baselineVariation.id);
                 if (iteration.updatedAt) {
                   expt.selectedIteration.updatedAt = iteration.updatedAt;
-                  expt.selectedIteration.updatedAtStr = format(iteration.updatedAt, 'YYYY-MM-DD HH:mm');
+                  expt.selectedIteration.updatedAtStr = format(iteration.updatedAt, 'yyyy-mm-dd hh:mm');
                 }
 
                 // update experiment original iterations
@@ -204,7 +204,7 @@ export class ExperimentationComponent implements OnInit, OnDestroy {
     this.experimentService.stopIteration(expt.id, expt.selectedIteration.id).subscribe(res => {
       if (res) {
         expt.selectedIteration.endTime = res.endTime;
-        expt.selectedIteration.dateTimeInterval = `${format(expt.selectedIteration.startTime, 'YYYY-MM-DD HH:mm')} - ${format(expt.selectedIteration.endTime, 'YYYY-MM-DD HH:mm')}`
+        expt.selectedIteration.dateTimeInterval = `${format(expt.selectedIteration.startTime, 'yyyy-mm-dd hh:mm')} - ${format(expt.selectedIteration.endTime, 'yyyy-mm-dd hh:mm')}`
         expt.status = ExperimentStatus.Paused;
       }
 
@@ -244,7 +244,7 @@ export class ExperimentationComponent implements OnInit, OnDestroy {
         expt.selectedIteration = this.processIteration({...expt.selectedIteration , ...res[0]}, expt.baselineVariation.id);
         if (res[0].updatedAt) {
           expt.selectedIteration.updatedAt = res[0].updatedAt;
-          expt.selectedIteration.updatedAtStr = format(res[0].updatedAt, 'YYYY-MM-DD HH:mm');
+          expt.selectedIteration.updatedAtStr = format(res[0].updatedAt, 'yyyy-mm-dd hh:mm');
         }
 
         this.setExptStatus(expt, res[0]);
@@ -299,10 +299,10 @@ export class ExperimentationComponent implements OnInit, OnDestroy {
     const winnerVariation = !!iterationResults.find(e => e.isWinner);
 
     const nowStr = (iteration.isFinish === true) ? "" : "(" + ($localize `:@@common.now:Now`) + ")";
-    const startStr = `${format(iteration.startTime, 'YYYY-MM-DD HH:mm')}`;
+    const startStr = `${format(iteration.startTime, 'yyyy-mm-dd hh:mm')}`;
     const endStr = `${iteration.endTime ?
-      format(iteration.endTime, 'YYYY-MM-DD HH:mm') :
-      format(new Date(), 'YYYY-MM-DD HH:mm')}  ${nowStr}`
+      format(iteration.endTime, 'yyyy-mm-dd hh:mm') :
+      format(new Date(), 'yyyy-mm-dd hh:mm')}  ${nowStr}`
       
     return {
       ...iteration,
@@ -368,7 +368,7 @@ export class ExperimentationComponent implements OnInit, OnDestroy {
         name: xAxisName,
         position: 'end',
         field: 'time',
-        scale: {type: "timeCat", nice: true, range: [0.05, 0.95], mask: 'YYYY-MM-DD HH:mm'}
+        scale: {type: "timeCat", nice: true, range: [0.05, 0.95], mask: 'yyyy-mm-dd hh:mm'}
       },
       yAxis: {
         name: yAxisName,
