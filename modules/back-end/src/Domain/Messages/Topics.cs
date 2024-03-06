ce8275f44f741aabf514257cb31bf9506eadf63b
deleteLater <mikcczhang@gmail.com>
2024-03-06 20:31:52 +0800

    normalize redis key

--- modules/back-end/src/Domain/Messages/Topics.cs
+++ modules/back-end/src/Domain/Messages/Topics.cs
@@ -2,11 +2,11 @@ namespace Domain.Messages;
 
 public static class Topics
 {
-    public const string EndUser = "featbit-endusers";
+    public const string EndUser = "featbit:endusers";
 
-    public const string FeatureFlagChange = "featbit-feature-flag-change";
+    public const string FeatureFlagChange = "featbit:featureflag.change";
 
-    public const string SegmentChange = "featbit-segment-change";
+    public const string SegmentChange = "featbit:segment.change";
 
-    public const string Insights = "featbit-insights";
+    public const string Insights = "featbit:insights";
 }
