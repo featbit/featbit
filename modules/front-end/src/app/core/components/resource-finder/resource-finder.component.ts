65a6c49a900010935fe137dd855f084c73764f06
deleteLater <mikcczhang@gmail.com>
2024-03-28 17:37:42 +0800

    make resources as input property

--- modules/front-end/src/app/core/components/resource-finder/resource-finder.component.ts
+++ modules/front-end/src/app/core/components/resource-finder/resource-finder.component.ts
@@ -1,36 +1,50 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+
+interface GroupedItem {
+  name: string;
+  items: Item[];
+}
+
+interface Item {
+  id: string;
+  name: string;
+}
 
 @Component({
   selector: 'resource-finder',
   templateUrl: './resource-finder.component.html',
   styleUrls: ['./resource-finder.component.less']
 })
-export class ResourceFinderComponent {
+export class ResourceFinderComponent implements OnInit {
   @Input()
   isVisible = false;
+  @Input()
+  resources: string[] = ['Projects', 'Environments', 'Flags', 'Segments']
 
-  resources = ['Projects', 'Environments', 'Flags', 'Segments'];
-
-  groupedItems = this.resources.map(resource => {
-    return {
-      name: resource,
-      items: [
-        {
-          id: "1",
-          name: 'Resource 1',
-        },
-        {
-          id: "2",
-          name: 'Resource 2',
-        },
-        {
-          id: "3",
-          name: 'Resource 3',
-        }
-      ]
-    };
-  });
+  groupedItems: GroupedItem[] = [];
 
   constructor() {
+  }
+
+  ngOnInit(): void {
+    this.groupedItems = this.resources.map(resource => {
+      return {
+        name: resource,
+        items: [
+          {
+            id: "1",
+            name: 'Resource 1',
+          },
+          {
+            id: "2",
+            name: 'Resource 2',
+          },
+          {
+            id: "3",
+            name: 'Resource 3',
+          }
+        ]
+      };
+    });
   }
 }
