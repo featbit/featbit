a3735e949fe5bf20565ccbe6258ceb1509b8fde6
deleteLater <mikcczhang@gmail.com>
2024-04-23 23:33:27 +0800

    refactor license component

--- modules/front-end/src/app/core/components/license/license.component.ts
+++ modules/front-end/src/app/core/components/license/license.component.ts
@@ -10,10 +10,34 @@ export class LicenseComponent {
 
   @Input() license: License;
 
+  features: LicenseFeatureEnum[] = [
+    LicenseFeatureEnum.Sso,
+    LicenseFeatureEnum.Schedule,
+    LicenseFeatureEnum.ChangeRequest,
+    LicenseFeatureEnum.MultiOrg,
+    LicenseFeatureEnum.GlobalUser
+  ];
+
   getLocalDate(date: number) {
     if (!date) return '';
     return new Date(date);
   }
 
   protected readonly LicenseFeatureEnum = LicenseFeatureEnum;
+
+  getFeatureName(feature: LicenseFeatureEnum): string {
+    // this.title = $localize`:@@relay-proxy.add-title:Add Relay Proxy`;
+    switch (feature) {
+      case LicenseFeatureEnum.Sso:
+        return $localize`:@@common.sso:Single Sign-On`;
+      case LicenseFeatureEnum.Schedule:
+        return $localize`:@@common.schedule:Schedule`;
+      case LicenseFeatureEnum.ChangeRequest:
+        return $localize`:@@common.change-request:Change Request`;
+      case LicenseFeatureEnum.MultiOrg:
+        return $localize`:@@common.multi-org:Multiple Organization`;
+      case LicenseFeatureEnum.GlobalUser:
+        return $localize`:@@common.global-user:Global User`;
+    }
+  }
 }
