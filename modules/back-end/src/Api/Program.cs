096aad31a4165f5cf93c6c408929804e7bf1419d
deleteLater <mikcczhang@gmail.com>
2024-09-19 23:07:44 +0800

    chore

--- modules/back-end/src/Api/Program.cs
+++ modules/back-end/src/Api/Program.cs
@@ -64,5 +64,4 @@ void InitializeSerilog()
 
 // https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-6.0#basic-tests-with-the-default-webapplicationfactory
 // Make the implicit Program class public so test projects can access it
-public partial class Program
-{ }
+public partial class Program { }
