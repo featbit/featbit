d4854f57cd9c9f675212a497867f984816427056
deleteLater <mikcczhang@gmail.com>
2025-02-24 17:52:40 +0800

    remove unused file

--- modules/back-end/src/Infrastructure/Persistence/EntityFrameworkCore/PostgresOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Infrastructure.Persistence.EntityFrameworkCore;
-
-public class PostgresOptions
-{
-    public const string Postgres = nameof(Postgres);
-
-    public string ConnectionString { get; set; } = string.Empty;
-}
