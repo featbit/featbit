93f87ec1f80c16c44130ca2178fd84f35eac513d
cosmos-explorer <lian.yang.work@gmail.com>
2025-03-03 23:12:47 +0100

    Update sql.py

--- modules/data-analytics/app/postgresql/models/event/sql.py
+++ modules/data-analytics/app/postgresql/models/event/sql.py
@@ -9,13 +9,6 @@ CREATE TABLE IF NOT EXISTS events
     timestamp TIMESTAMP NOT NULL
 );
 
-CREATE INDEX idx_events_distinct_id ON events (distinct_id);
-CREATE INDEX idx_events_env_id ON events (env_id);
-CREATE INDEX idx_events_event ON events (event);
-CREATE INDEX idx_events_timestamp ON events (timestamp);
-
-CREATE INDEX idx_events_tag0_expr ON events ((properties->>'tag_0'));
-CREATE INDEX idx_events_tag1_expr ON events ((properties->>'tag_1'));
-CREATE INDEX idx_events_tag2_expr ON events ((properties->>'tag_2'));
-CREATE INDEX idx_events_tag3_expr ON events ((properties->>'tag_3'));
+CREATE INDEX idx_events_combined
+ON events (distinct_id, event, env_id, timestamp);
 """
