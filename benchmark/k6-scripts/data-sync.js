cb49680573800fc992b12133332e4b414585b641
deleteLater <mikcczhang@gmail.com>
2025-05-28 21:26:19 +0800

    🏗 infra: update outdated k6 scripts (#753)
    * update data
    
    * add smoke, track, data-sync scripts
    
    * remove unused scripts
    
    * update README.md
    
    * update README.md
    
    * refactor
    
    * update docs
    
    * update smoke.js

---
+++ benchmark/k6-scripts/data-sync.js
@@ -0,0 +1,134 @@
+import exec from 'k6/execution';
+import { WebSocket } from 'k6/experimental/websockets';
+import { Counter, Trend } from "k6/metrics";
+
+import { STREAMING_CLIENT_URL } from './env.js';
+import { htmlReport } from "./k6-reporter.js";
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.4/index.js'
+
+const PING_INTERVAL = 18 * 1000; // 18 seconds
+
+const LATENCY = new Trend("latency");
+const PING_COUNTER = new Counter("ping_sent");
+const PONG_COUNTER = new Counter("pong_received");
+const ERROR_COUNTER = new Counter("error_occurred");
+const DATA_SYNC_REQUEST_COUNTER = new Counter("data_sync_request");
+const DATA_SYNC_SUCCESS_COUNTER = new Counter("data_sync_success");
+const CLOSED_CONNECTION_COUNTER = new Counter("connection_closed");
+
+const PING_MESSAGE = JSON.stringify({
+  messageType: 'ping',
+  data: {}
+});
+
+// ramping up requests per second
+const THROUGHPUT = __ENV.THROUGHPUT ? parseInt(__ENV.THROUGHPUT) : 100;
+
+// ramping up duration in seconds
+const RAMPING_UP_DURATION_SECONDS = __ENV.RAMPING_UP_DURATION_SECONDS ? parseInt(__ENV.RAMPING_UP_DURATION_SECONDS) : 10;
+
+// the target number of VUs based on the throughput and ramping up duration
+const RAMPING_UP_TARGET = THROUGHPUT * RAMPING_UP_DURATION_SECONDS;
+
+// duration in seconds to keep the peak number of VUs
+const KEEP_PEAK_DURATION_SECONDS = __ENV.KEEP_PEAK_DURATION_SECONDS ? parseInt(__ENV.KEEP_PEAK_DURATION_SECONDS) : 60;
+
+// duration in seconds to ramp down the VUs
+const RAMPING_DOWN_DURATION_SECONDS = __ENV.RAMPING_DOWN_DURATION_SECONDS ? parseInt(__ENV.RAMPING_DOWN_DURATION_SECONDS) : 10;
+
+// total session duration in seconds
+const SESSION_DURATION_SECONDS = RAMPING_UP_DURATION_SECONDS + KEEP_PEAK_DURATION_SECONDS + RAMPING_DOWN_DURATION_SECONDS;
+
+export const options = {
+  scenarios: {
+    load_testing: {
+      executor: "ramping-vus",
+      startVus: 0,
+      stages: [
+        { duration: `${RAMPING_UP_DURATION_SECONDS}s`, target: RAMPING_UP_TARGET },
+        { duration: `${KEEP_PEAK_DURATION_SECONDS}s`, target: RAMPING_UP_TARGET },
+        { duration: `${RAMPING_DOWN_DURATION_SECONDS}s`, target: 0 }
+      ]
+    }
+  }
+}
+
+export default function () {
+  const ws = new WebSocket(STREAMING_CLIENT_URL);
+  const keyId = `k6-vu-${exec.vu.idInTest}`;
+
+  let dataSyncSendTime = 0;
+  let pingIntervalTask = null;
+  let sessionEndTask = null;
+
+  ws.onopen = () => {
+    const FULL_DATA_SYNC_MESSAGE = JSON.stringify({
+      messageType: 'data-sync',
+      data: {
+        user: {
+          keyId: keyId,
+          name: keyId,
+          customizedProperties: [
+            { name: 'email', value: `${keyId}@k6.com` },
+            { name: 'location', value: '127.0.0.1' },
+          ]
+        },
+        timestamp: 0
+      }
+    });
+
+    dataSyncSendTime = Date.now();
+    ws.send(FULL_DATA_SYNC_MESSAGE);
+    DATA_SYNC_REQUEST_COUNTER.add(1);
+
+    pingIntervalTask = setInterval(() => {
+      ws.send(PING_MESSAGE);
+      PING_COUNTER.add(1);
+    }, PING_INTERVAL);
+
+    sessionEndTask = setTimeout(() => {
+      ws.close();
+    }, SESSION_DURATION_SECONDS * 1000);
+  }
+
+  ws.onmessage = (e) => {
+    const message = JSON.parse(e.data);
+
+    if (message.messageType === 'data-sync' && message.data.eventType === "full" && message.data.userKeyId === keyId) {
+      const latency = Date.now() - dataSyncSendTime;
+      LATENCY.add(latency);
+
+      DATA_SYNC_SUCCESS_COUNTER.add(1);
+    }
+
+    if (message.messageType === "pong") {
+      PONG_COUNTER.add(1);
+    }
+  };
+
+  ws.onerror = (err) => {
+    if (Object.keys(err).length === 0) {
+      return;
+    }
+
+    ERROR_COUNTER.add(1);
+  }
+
+  ws.onclose = () => {
+    clearInterval(pingIntervalTask);
+    clearTimeout(sessionEndTask);
+
+    CLOSED_CONNECTION_COUNTER.add(1);
+  }
+}
+
+export function handleSummary(data) {
+  console.log(`Throughput: ${THROUGHPUT}/s, ${RAMPING_UP_TARGET} max VUs, ${RAMPING_UP_DURATION_SECONDS}s duration for ramping up phase and keep stable for ${KEEP_PEAK_DURATION_SECONDS}s`);
+
+  const report_name = `data-sync-summary-${THROUGHPUT}.html`;
+
+  return {
+    'stdout': textSummary(data, { indent: ' ', enableColors: true }),
+    [report_name]: htmlReport(data)
+  };
+}
---
+++ benchmark/k6-scripts/smoke.js
@@ -0,0 +1,137 @@
+import { WebSocket } from 'k6/experimental/websockets';
+
+import { STREAMING_SERVER_URL, STREAMING_CLIENT_URL } from './env.js';
+import { check } from 'k6';
+
+const WebSocketReadyState = {
+  CONNECTING: 0, // Socket has been created. The connection is not yet open.
+  OPEN: 1,        // The connection is open and ready to communicate.
+  CLOSING: 2,     // The connection is in the process of closing.
+  CLOSED: 3       // The connection is closed or couldn't be opened.
+};
+
+const CLIENT_DATA_SYNC_MESSAGE = JSON.stringify({
+  messageType: 'data-sync',
+  data: {
+    user: {
+      keyId: `k6-tester`,
+      name: `K6 Tester`,
+      customizedProperties: [
+        { name: 'email', value: `k6-tester@k6.com` },
+        { name: 'location', value: '127.0.0.1' },
+      ]
+    },
+    timestamp: 0
+  }
+});
+
+const SERVER_DATA_SYNC_MESSAGE = JSON.stringify({
+  messageType: 'data-sync',
+  data: {
+    timestamp: 0
+  }
+});
+
+const CONNECTION_DURATION_SECONDS = 2;
+
+// wait a bit longer than the connection duration to ensure the WebSocket is closed
+const WEBSOCKET_CLOSE_WAIT_TIME = (CONNECTION_DURATION_SECONDS + 2) * 1000;
+
+export default async function () {
+  console.log(`Starting WebSocket client test for ${CONNECTION_DURATION_SECONDS} seconds...`);
+
+  startWs(STREAMING_CLIENT_URL, 'client');
+  // wait for the client WebSocket to finish
+  await new Promise(resolve => setTimeout(resolve, WEBSOCKET_CLOSE_WAIT_TIME));
+
+  console.log(`Client WebSocket finished.\n\n`);
+
+  console.log(`Starting WebSocket server test for ${CONNECTION_DURATION_SECONDS} seconds...`);
+
+  startWs(STREAMING_SERVER_URL, 'server');
+  // wait for the server WebSocket to finish
+  await new Promise(resolve => setTimeout(resolve, WEBSOCKET_CLOSE_WAIT_TIME));
+
+  console.log(`Server WebSocket finished.\n\n`);
+}
+
+function startWs(url, type) {
+  const ws = new WebSocket(url);
+
+  let echoSent = false;
+  let echoReceived = false;
+
+  let pingSent = false;
+  let pongReceived = false;
+
+  let dataSyncSent = false;
+  let dataSyncReceived = false;
+
+  ws.onopen = () => {
+    check(ws, {
+      'is connected': (ws) => ws.readyState === WebSocketReadyState.OPEN,
+    });
+
+    console.log(`WebSocket connection established: ${url}. Last for ${CONNECTION_DURATION_SECONDS} seconds.`);
+
+    const echoMessage = {
+      messageType: 'echo',
+      data: Date.now()
+    };
+    ws.send(JSON.stringify(echoMessage));
+
+    echoSent = true;
+    console.log(`Echo message sent from client: ${echoMessage.data}`);
+
+    const pingMessage = {
+      messageType: 'ping',
+      data: {}
+    };
+    ws.send(JSON.stringify(pingMessage));
+
+    pingSent = true;
+    console.log(`Ping message sent from client: ${JSON.stringify(pingMessage)}`);
+
+    const dataSyncMessage = type === 'server' ? SERVER_DATA_SYNC_MESSAGE : CLIENT_DATA_SYNC_MESSAGE;
+    ws.send(dataSyncMessage);
+
+    dataSyncSent = true;
+    console.log(`Data sync message sent from client: ${dataSyncMessage}`);
+  };
+
+  ws.onmessage = (msg) => {
+    const data = JSON.parse(msg.data);
+    if (data.messageType === 'echo') {
+      console.log(`Received echo message from server: ${data.data}`);
+      echoReceived = true;
+    }
+    if (data.messageType === 'pong') {
+      console.log(`Received pong message from server: ${JSON.stringify(data)}`);
+      pongReceived = true;
+    }
+    if (data.messageType === 'data-sync') {
+      console.log(`Received data sync message from server: ${JSON.stringify(data.data)}`);
+      dataSyncReceived = true;
+    }
+  };
+
+  const closeTimeout = setTimeout(() => {
+    ws.close();
+  }, CONNECTION_DURATION_SECONDS * 1000);
+
+  ws.onclose = () => {
+    console.log(`WebSocket connection closed`);
+
+    clearTimeout(closeTimeout);
+    check(ws, {
+      'is closed': (ws) => ws.readyState === WebSocketReadyState.CLOSED,
+      'echo message sent': () => echoSent,
+      'echo message received': () => echoReceived,
+      'ping message sent': () => pingSent,
+      'pong message received': () => pongReceived,
+      'data sync message sent': () => dataSyncSent,
+      'data sync message received': () => dataSyncReceived,
+    });
+  };
+}
+
