54baf0e975929398a6bd807a27fd7e90fbf70b2c
cosmos-explorer <88151306+cosmos-explorer@users.noreply.github.com>
2025-06-02 09:52:17 +0200

    ✨ feat: added permission for creating organization (#759)
    * feat: added permission for creating organization
    
    * typo
    
    ---------
    
    Co-authored-by: deleteLater <mikcczhang@gmail.com>

---
+++ infra/mongodb/docker-entrypoint-initdb.d/v5.0.5.js
@@ -0,0 +1,33 @@
+const dbName = "featbit";
+print('use', dbName, 'database')
+db = db.getSiblingDB(dbName)
+
+const administratorPolicyId = UUID("3e961f0f-6fd4-4cf4-910f-52d356f8cc08")
+
+function getUUIDString() {
+    return UUID().toString().split('"')[1];
+}
+
+const newStatement = {
+  _id: getUUIDString(),
+  resourceType: "organization",
+  effect: "allow",
+  actions: [
+    "UpdateOrgName",
+    "UpdateOrgDefaultUserPermissions",
+    "CreateOrg"
+  ],
+  resources: ["organization/*"]
+};
+
+// Step 1: Pull any existing organization statement
+db.Policies.updateOne(
+  { _id: administratorPolicyId },
+  { $pull: { statements: { resourceType: "organization" } } }
+);
+
+// Step 2: Push the new statement
+db.Policies.updateOne(
+  { _id: administratorPolicyId },
+  { $push: { statements: newStatement } }
+);
