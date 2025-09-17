afbb42977c6a07622c29f9d827f0153143e27c50
deleteLater <mikcczhang@gmail.com>
2025-09-17 19:09:37 +0800

    fix env file paths in docker-entrypoint.sh

--- modules/front-end/docker-entrypoint.sh
+++ modules/front-end/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 # Abort on any error (including if wait-for-it fails).
 set -e
 
-envsubst < /usr/share/nginx/featbit/en/assets/env.template.js > /usr/share/nginx/featbit/en/assets/env.js
-envsubst < /usr/share/nginx/featbit/zh/assets/env.template.js > /usr/share/nginx/featbit/zh/assets/env.js
+envsubst < /usr/share/nginx/featbit/browser/en/assets/env.template.js > /usr/share/nginx/featbit/browser/en/assets/env.js
+envsubst < /usr/share/nginx/featbit/browser/zh/assets/env.template.js > /usr/share/nginx/featbit/browser/zh/assets/env.js
 exec nginx -g 'daemon off;'
