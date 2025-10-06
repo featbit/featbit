ad343505c562eb3df7a95e146580aa23fd81b710
cosmos-explorer <88151306+cosmos-explorer@users.noreply.github.com>
2025-10-06 10:56:35 +0200

    Update docker-entrypoint.sh for nginx configuration
    Added logic to select nginx configuration based on BASE_HREF and clean up template files.

--- modules/front-end/docker-entrypoint.sh
+++ modules/front-end/docker-entrypoint.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-# Abort on any error (including if wait-for-it fails).
+# Abort on any error
 set -e
 
 # Remove trailing slash if present
 BASE_HREF="${BASE_HREF%/}"
+
+# Select and process nginx configuration based on BASE_HREF
+if [ -n "$BASE_HREF" ] && [ "$BASE_HREF" != "" ]; then
+  echo "Using nginx.base_href.conf with BASE_HREF=$BASE_HREF"
+  envsubst '$BASE_HREF' < /etc/nginx/conf.d/nginx.base_href.conf > /etc/nginx/conf.d/default.conf
+else
+  echo "Using nginx.conf (default configuration)"
+  cp /etc/nginx/conf.d/nginx.default.conf /etc/nginx/conf.d/default.conf
+fi
+
+# Remove the template files to avoid confusion
+rm -f /etc/nginx/conf.d/nginx.base_href.conf /etc/nginx/conf.d/nginx.default.conf
 
 # Process each locale directory
 for locale in /usr/share/nginx/featbit/*; do
