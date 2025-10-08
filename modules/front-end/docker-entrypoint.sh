1cf4cb60dc60e6c2a9c996f14ceec513a4f55453
cosmos-explorer <88151306+cosmos-explorer@users.noreply.github.com>
2025-10-08 16:04:07 +0200

    ✨ feat: make the base_href configurable in the UI (#796)
    * feat: make the base_href configurable in the UI
    
    * Update docker-entrypoint.sh
    
    * nginx config file for when base_href is provided
    
    * Rename nginx configuration file in Dockerfile
    
    * Rename Nginx configuration file in Dockerfile
    
    * Update docker-entrypoint.sh for nginx configuration
    
    Added logic to select nginx configuration based on BASE_HREF and clean up template files.
    
    * Add files via upload
    
    * removed language if in nginx conf file
    
    * Update nginx.base_href.conf
    
    * Update docker-entrypoint.sh

--- modules/front-end/docker-entrypoint.sh
+++ modules/front-end/docker-entrypoint.sh
@@ -1,8 +1,38 @@
 #!/bin/sh
 
-# Abort on any error (including if wait-for-it fails).
+# Abort on any error
 set -e
 
-envsubst < /usr/share/nginx/featbit/en/assets/env.template.js > /usr/share/nginx/featbit/en/assets/env.js
-envsubst < /usr/share/nginx/featbit/zh/assets/env.template.js > /usr/share/nginx/featbit/zh/assets/env.js
+# Remove trailing slash if present
+BASE_HREF="${BASE_HREF%/}"
+
+# Select and process nginx configuration based on BASE_HREF
+if [ -n "$BASE_HREF" ]; then
+  echo "Using nginx.base_href.conf with BASE_HREF=$BASE_HREF"
+  envsubst '$BASE_HREF' < /etc/nginx/conf.d/nginx.base_href.conf > /etc/nginx/conf.d/default.conf
+else
+  echo "Using nginx.conf (default configuration)"
+  cp /etc/nginx/conf.d/nginx.default.conf /etc/nginx/conf.d/default.conf
+fi
+
+# Remove the template files to avoid confusion
+rm -f /etc/nginx/conf.d/nginx.base_href.conf /etc/nginx/conf.d/nginx.default.conf
+
+# Process each locale directory
+for locale in /usr/share/nginx/featbit/*; do
+  if [ -d "$locale" ]; then
+    lang=$(basename "$locale")
+
+    # Update <base href="..."> inside index.html
+    if [ -f "$locale/index.html" ]; then
+      sed -i "s|<base href=\"/${lang}/\"|<base href=\"${BASE_HREF}/${lang}/\"|g" "$locale/index.html"
+    fi
+
+    # Generate env.js from env.template.js if available
+    if [ -f "$locale/assets/env.template.js" ]; then
+      envsubst < "$locale/assets/env.template.js" > "$locale/assets/env.js"
+    fi
+  fi
+done
+
 exec nginx -g 'daemon off;'
