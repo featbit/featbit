6bcb5dba36d83b93a150f10cb5a5dbe3b42f4738
deleteLater <mikcczhang@gmail.com>
2025-10-09 10:33:11 +0800

    publish feature flag change notification on setting tags

--- modules/back-end/src/Application/FeatureFlags/SetTags.cs
+++ modules/back-end/src/Application/FeatureFlags/SetTags.cs
@@ -1,6 +1,5 @@
 using Application.Users;
 using Domain.AuditLogs;
-using Domain.FlagRevisions;
 
 namespace Application.FeatureFlags;
 
@@ -13,38 +12,18 @@ public class SetTags : IRequest<bool>
     public string[] Tags { get; set; }
 }
 
-public class SetTagsHandler : IRequestHandler<SetTags, bool>
+public class SetTagsHandler(IFeatureFlagService service, ICurrentUser currentUser, IPublisher publisher)
+    : IRequestHandler<SetTags, bool>
 {
-    private readonly IFeatureFlagService _service;
-    private readonly IFlagRevisionService _flagRevisionService;
-    private readonly ICurrentUser _currentUser;
-    private readonly IAuditLogService _auditLogService;
-
-    public SetTagsHandler(
-        IFeatureFlagService service,
-        IFlagRevisionService flagRevisionService,
-        ICurrentUser currentUser,
-        IAuditLogService auditLogService)
-    {
-        _service = service;
-        _flagRevisionService = flagRevisionService;
-        _currentUser = currentUser;
-        _auditLogService = auditLogService;
-    }
-
     public async Task<bool> Handle(SetTags request, CancellationToken cancellationToken)
     {
-        var flag = await _service.GetAsync(request.EnvId, request.Key);
-        var dataChange = flag.SetTags(request.Tags, _currentUser.Id);
-        await _service.UpdateAsync(flag);
+        var flag = await service.GetAsync(request.EnvId, request.Key);
+        var dataChange = flag.SetTags(request.Tags, currentUser.Id);
+        await service.UpdateAsync(flag);
 
-        // write audit log
-        var auditLog = AuditLog.For(flag, Operations.Update, dataChange, string.Empty, _currentUser.Id);
-        await _auditLogService.AddOneAsync(auditLog);
-
-        // add flag revision
-        var revision = new FlagRevision(flag, string.Empty);
-        await _flagRevisionService.AddOneAsync(revision);
+        // publish on feature flag change notification
+        var notification = new OnFeatureFlagChanged(flag, Operations.Update, dataChange, currentUser.Id);
+        await publisher.Publish(notification, cancellationToken);
 
         return true;
     }
