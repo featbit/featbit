4dc6b5a97af23ef01eccc522a90d7076ae490e02
cosmos-explorer <88151306+cosmos-explorer@users.noreply.github.com>
2025-10-26 08:24:15 +0100

    more fix

--- modules/front-end/src/app/shared/utils/index.ts
+++ modules/front-end/src/app/shared/utils/index.ts
@@ -4,7 +4,7 @@ import { USER_IS_IN_SEGMENT, USER_IS_NOT_IN_SEGMENT } from "@shared/constants";
 import { UserOriginEnum } from "@features/safe/workspaces/types/profiles";
 
 export function getPathPrefix() {
-  return location.pathname.match(/^(?<locale>\/en\/|\/zh\/)/i)?.groups['locale'] || '/';
+  return document.querySelector('base')?.getAttribute('href');
 }
 
 export function getProfile(): IProfile | null {
