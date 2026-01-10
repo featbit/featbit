eda581a288ca75172dc2eb6e162939ad95b0ce00
deleteLater <mikcczhang@gmail.com>
2026-01-10 17:37:33 +0800

    refactor member drawer implementation

--- modules/front-end/src/app/core/components/member-drawer/member-drawer.component.ts
+++ modules/front-end/src/app/core/components/member-drawer/member-drawer.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, Input, Output, EventEmitter, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { phoneNumberOrEmailValidator } from "@utils/form-validators";
 import { PolicyService } from "@services/policy.service";
@@ -7,6 +7,7 @@ import { IPagedPolicy, PolicyFilter } from "@features/safe/iam/types/policy";
 import { GroupListFilter, IPagedGroup } from "@features/safe/iam/types/group";
 import { GroupService } from "@services/group.service";
 import {OrganizationService} from "@services/organization.service";
+import { finalize } from "rxjs/operators";
 
 @Component({
     selector: 'app-member-drawer',
@@ -14,111 +15,112 @@ import {OrganizationService} from "@services/organization.service";
     styleUrls: ['./member-drawer.component.less'],
     standalone: false
 })
-export class MemberDrawerComponent {
+export class MemberDrawerComponent implements OnInit {
+  private _visible: boolean = false;
+  @Input()
+  get visible(): boolean {
+    return this._visible;
+  }
+
+  set visible(value: boolean) {
+    this._visible = value;
+    if (value) {
+      this.form.reset();
+    }
+  }
+
+  @Output() close: EventEmitter<boolean> = new EventEmitter();
+
+  form: FormGroup;
+
+  private formBuilder: FormBuilder = inject(FormBuilder);
+  private organizationService: OrganizationService = inject(OrganizationService);
+  private message: NzMessageService = inject(NzMessageService);
+  private policyService: PolicyService = inject(PolicyService);
+  private groupService: GroupService = inject(GroupService);
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: [ '', [ phoneNumberOrEmailValidator, Validators.required ] ],
+      permissions: this.formBuilder.group({
+        policyId: [ [] ],
+        groupId: [ [] ]
+      }, { validators: this.permissionsValidator })
+    });
+
+    this.loadPolicies();
+    this.loadGroups();
+  }
+
+  permissionsValidator: ValidatorFn = (group: FormGroup) => {
+    const policyId = group.get('policyId').value;
+    const groupId = group.get('groupId').value;
+
+    if (!policyId && !groupId) {
+      return { invalid: true };
+    }
+
+    return null;
+  };
 
   isPoliciesLoading = true;
-  policyFilter: PolicyFilter = new PolicyFilter(null, 1, 50);
-
+  policyFilter: PolicyFilter = new PolicyFilter(null, 1, 20);
   policies: IPagedPolicy = {
     items: [],
     totalCount: 0
   };
+  loadPolicies(query?: string) {
+    this.policyFilter.name = query || '';
+
+    this.isPoliciesLoading = true;
+    this.policyService.getList(this.policyFilter)
+    .pipe(finalize(() => this.isPoliciesLoading = false))
+      .subscribe({
+        next: policies => this.policies = policies,
+        error: () => this.message.error($localize`:@@iam.teams.failed-to-load-policies:Failed to load policies`)
+      });
+  }
 
   isGroupsLoading = true;
-  groupFilter: GroupListFilter = new GroupListFilter(null, 1, 50);
+  groupFilter: GroupListFilter = new GroupListFilter(null, 1, 20);
   groups: IPagedGroup = {
     items: [],
     totalCount: 0
   };
-
-  @Input() visible: boolean = false;
-  @Output() close: EventEmitter<boolean> = new EventEmitter();
-
-  memberForm: FormGroup;
-  constructor(
-    private fb: FormBuilder,
-    private organizationService: OrganizationService,
-    private message: NzMessageService,
-    private policyService: PolicyService,
-    private groupService: GroupService
-  ) {
-    this.getPolicies();
-    this.getGroups();
-
-    this.memberForm = this.fb.group({
-      email: ['', [phoneNumberOrEmailValidator, Validators.required]],
-      policyId: [''],
-      groupId: ['']
-    });
-  }
-
-  getPolicies(query?: string) {
-    this.policyFilter.name = query || '';
-
-    this.isPoliciesLoading = true;
-    this.policyService.getList(this.policyFilter).subscribe(policies => {
-      this.policies = policies;
-      this.isPoliciesLoading = false;
-    }, () => this.isPoliciesLoading = false);
-  }
-
-  getGroups(query?: string) {
+  loadGroups(query?: string) {
     this.groupFilter.name = query || '';
 
     this.isGroupsLoading = true;
-    this.groupService.getList(this.groupFilter).subscribe(groups => {
-      this.groups = groups;
-      this.isGroupsLoading = false;
-    }, () => this.isGroupsLoading = false);
+    this.groupService.getList(this.groupFilter)
+    .pipe(finalize(() => this.isGroupsLoading = false))
+      .subscribe({
+        next: groups => this.groups = groups,
+        error: () => this.message.error($localize`:@@iam.teams.failed-to-load-groups:Failed to load groups`)
+      });
+  }
+
+  isAddingUser: boolean = false;
+  doSubmit() {
+    const { email, permissions } = this.form.value;
+    const payload = {
+      email,
+      policyIds: permissions.policyId ? [permissions.policyId] : [],
+      groupIds: permissions.groupId ? [permissions.groupId] : []
+    }
+
+    this.isAddingUser = true;
+    this.organizationService.addUser(payload)
+    .pipe(finalize(() => this.isAddingUser = false))
+    .subscribe({
+      next: () => {
+        this.message.success($localize`:@@common.operation-success:Operation succeeded`);
+        this.close.emit(true);
+      },
+      error: () => this.message.error($localize`:@@common.operation-failed:Operation failed`)
+    });
   }
 
   onClose() {
-    this.isPermissionInvalid = false;
-    this.close.emit();
-  }
-
-  isPermissionInvalid = false;
-  validatePermissions(): boolean {
-    let { policyId, groupId } = this.memberForm.value;
-
-    if ((policyId === null || policyId.length === 0) && (groupId === null || groupId.length === 0)) {
-      this.isPermissionInvalid = true;
-      return false;
-    }
-
-    this.isPermissionInvalid = false;
-    return true;
-  }
-
-  isLoading: boolean = false;
-  doSubmit() {
-    if (this.memberForm.invalid) {
-      for (const i in this.memberForm.controls) {
-        this.memberForm.controls[i].markAsDirty();
-        this.memberForm.controls[i].updateValueAndValidity();
-      }
-      return;
-    }
-
-    if (!this.validatePermissions()) {
-      return;
-    }
-
-    let { policyId, groupId, email } = this.memberForm.value;
-    const policyIds = !!policyId ? [policyId] : [];
-    const groupIds = !!groupId ? [groupId] : [];
-    const method = 'Email';
-
-    this.isLoading = true;
-    this.organizationService.addUser({ method, email, policyIds, groupIds }).subscribe(
-      () => {
-        this.isLoading = false;
-        this.close.emit(true);
-        this.message.success($localize `:@@common.operation-success:Operation succeeded`);
-      },
-      _ => {
-        this.isLoading = false;
-      }
-    )
+    this.close.emit(false);
   }
 }
