7783fe02d30bf2897573a16464e5246d6edca1c1
deleteLater <mikcczhang@gmail.com>
2026-04-02 10:59:51 +0800

    i18n

--- modules/front-end/src/app/features/safe/segments/details/setting/setting.component.ts
+++ modules/front-end/src/app/features/safe/segments/details/setting/setting.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {
-  getSegmentRN,
   ISegment,
   ISegmentFlagReference,
   Segment,
