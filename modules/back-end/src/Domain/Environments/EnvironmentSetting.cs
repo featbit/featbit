c453a4e047904c14f38770b8ad4d93c8c878c936
deleteLater <mikcczhang@gmail.com>
2026-05-26 10:58:15 +0800

    add missing changes

--- modules/back-end/src/Domain/Environments/EnvironmentSetting.cs
+++ modules/back-end/src/Domain/Environments/EnvironmentSetting.cs
@@ -2,9 +2,7 @@ namespace Domain.Environments;
 
 public class EnvironmentSetting
 {
-    /// <summary>
-    /// Whether to require users to provide a comment when changing a flag value in the environment.
-    /// This is used for audit and change tracking purposes.
-    /// </summary>
-    public bool RequireChangeComment { get; set; }
+    public string Key { get; set; }
+
+    public string Value { get; set; }
 }
