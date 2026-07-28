6fe15ae5ede1a048bd5d4c1ea323ba18c2979308
cosmos-explorer <88151306+cosmos-explorer@users.noreply.github.com>
2026-07-28 09:05:12 +0200

    moved changes to index.css to another file

--- modules/front-end-v2/src/main.tsx
+++ modules/front-end-v2/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client"
 
 import { App } from "@/app/app"
 import "./index.css"
+import "./styles/global-overrides.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
