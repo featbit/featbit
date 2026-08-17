e51cac4c38b5fb4b951b614f16bdb4696066f40a
deleteLater <mikcczhang@gmail.com>
2026-08-17 21:39:54 +0800

    optimize MongoDB insight aggregation

--- modules/back-end/src/Infrastructure/Services/MongoDb/InsightService.cs
+++ modules/back-end/src/Infrastructure/Services/MongoDb/InsightService.cs
@@ -1,6 +1,7 @@
 using Application.FeatureFlags;
 using Application.Insights;
 using Domain.Experiments;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Infrastructure.Services.MongoDb;
@@ -33,54 +34,86 @@ public class InsightService(MongoDbClient mongoDb) : IInsightService
         var start = DateTimeOffset.FromUnixTimeMilliseconds(filter.From).UtcDateTime;
         var end = DateTimeOffset.FromUnixTimeMilliseconds(filter.To).UtcDateTime;
 
+        var unit = filter.IntervalType switch
+        {
+            IntervalType.Month => "month",
+            IntervalType.Week => "week",
+            IntervalType.Day => "day",
+            IntervalType.Hour => "hour",
+            IntervalType.Minute => "minute",
+
+            _ => throw new ArgumentException($"Unsupported interval type: {filter.IntervalType}",
+                nameof(filter.IntervalType))
+        };
+
+        var dateTrunc = new BsonDocument
+        {
+            { "date", "$exposedAt" },
+            { "unit", unit },
+            { "timezone", "UTC" }
+        };
+
+        if (filter.IntervalType == IntervalType.Week)
+        {
+            dateTrunc.Add("startOfWeek", "monday");
+        }
+
+        PipelineDefinition<ExperimentExposureEvent, BsonDocument> pipeline = new BsonDocument[]
+        {
+            new("$match", new BsonDocument
+            {
+                { "envId", new BsonBinaryData(envId, GuidRepresentation.Standard) },
+                { "flagKey", filter.FeatureFlagKey },
+                { "exposedAt", new BsonDocument { { "$gte", start }, { "$lte", end } } },
+                { "variationId", new BsonDocument { { "$type", "string" }, { "$regex", "\\S" } } }
+            }),
+            new("$group", new BsonDocument
+            {
+                {
+                    "_id",
+                    new BsonDocument
+                    {
+                        { "bucket", new BsonDocument("$dateTrunc", dateTrunc) },
+                        { "variationId", "$variationId" }
+                    }
+                },
+                { "count", new BsonDocument("$sum", 1) }
+            }),
+            new("$sort", new BsonDocument
+            {
+                { "_id.bucket", 1 },
+                { "_id.variationId", 1 }
+            }),
+            new("$group", new BsonDocument
+            {
+                { "_id", "$_id.bucket" },
+                {
+                    "variations",
+                    new BsonDocument("$push", new BsonDocument
+                    {
+                        { "id", "$_id.variationId" },
+                        { "val", "$count" }
+                    })
+                }
+            }),
+            new("$sort", new BsonDocument("_id", 1))
+        };
+
         var docs = await mongoDb.CollectionOf<ExperimentExposureEvent>()
-            .Find(x =>
-                x.EnvId == envId &&
-                x.FlagKey == filter.FeatureFlagKey &&
-                x.ExposedAt >= start &&
-                x.ExposedAt <= end &&
-                x.VariationId != null)
+            .Aggregate(pipeline)
             .ToListAsync();
 
-        return docs
-            .Select(doc => new
-            {
-                Bucket = Truncate(doc.ExposedAt, filter.IntervalType),
-                doc.VariationId
-            })
-            .Where(x => !string.IsNullOrWhiteSpace(x.VariationId))
-            .GroupBy(x => x.Bucket)
-            .OrderBy(x => x.Key)
-            .Select(group => new Insight
-            {
-                Time = group.Key.ToString("O"),
-                Variations = group
-                    .GroupBy(x => x.VariationId)
-                    .Select(x => new VariationInsights { Id = x.Key, Val = x.Count() })
-                    .ToArray()
-            })
-            .ToArray();
-    }
-
-    private static DateTime Truncate(DateTime timestamp, string intervalType)
-    {
-        var utc = timestamp.Kind == DateTimeKind.Utc ? timestamp : timestamp.ToUniversalTime();
-
-        return intervalType switch
+        return docs.Select(doc => new Insight
         {
-            IntervalType.Month => new DateTime(utc.Year, utc.Month, 1, 0, 0, 0, DateTimeKind.Utc),
-            IntervalType.Week => StartOfWeek(utc),
-            IntervalType.Day => new DateTime(utc.Year, utc.Month, utc.Day, 0, 0, 0, DateTimeKind.Utc),
-            IntervalType.Hour => new DateTime(utc.Year, utc.Month, utc.Day, utc.Hour, 0, 0, DateTimeKind.Utc),
-            IntervalType.Minute => new DateTime(utc.Year, utc.Month, utc.Day, utc.Hour, utc.Minute, 0, DateTimeKind.Utc),
-            _ => throw new ArgumentException($"Unsupported interval type: {intervalType}", nameof(intervalType))
-        };
-    }
-
-    private static DateTime StartOfWeek(DateTime utc)
-    {
-        var daysSinceMonday = ((int)utc.DayOfWeek + 6) % 7;
-        var monday = utc.Date.AddDays(-daysSinceMonday);
-        return DateTime.SpecifyKind(monday, DateTimeKind.Utc);
+            Time = doc["_id"].ToUniversalTime().ToString("O"),
+            Variations = doc["variations"].AsBsonArray
+                .Select(x => x.AsBsonDocument)
+                .Select(x => new VariationInsights
+                {
+                    Id = x["id"].AsString,
+                    Val = x["val"].ToInt32()
+                })
+                .ToArray()
+        }).ToArray();
     }
 }
