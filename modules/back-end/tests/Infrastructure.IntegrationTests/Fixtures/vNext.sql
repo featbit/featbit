38e9c77c5e536ca5ee77fb019ec78201d9440950
deleteLater <mikcczhang@gmail.com>
2026-09-18 13:30:09 +0800

    sync sql changes

--- modules/back-end/tests/Infrastructure.IntegrationTests/Fixtures/vNext.sql
+++ modules/back-end/tests/Infrastructure.IntegrationTests/Fixtures/vNext.sql
@@ -59,7 +59,6 @@ CREATE TABLE IF NOT EXISTS experiment_exposure_events (
     variation_id character varying(256) NOT NULL,
     variation_value character varying(512),
     exposed_at timestamp with time zone NOT NULL,
-    properties jsonb,
     created_at timestamp with time zone NOT NULL,
     CONSTRAINT pk_experiment_exposure_events PRIMARY KEY (id)
 );
@@ -84,8 +83,8 @@ CREATE TABLE IF NOT EXISTS experiment_metric_events (
     event_name character varying(256) NOT NULL,
     event_type character varying(64) NOT NULL,
     numeric_value double precision NOT NULL,
+    application_type character varying(64),
     occurred_at timestamp with time zone NOT NULL,
-    properties jsonb,
     created_at timestamp with time zone NOT NULL,
     CONSTRAINT pk_experiment_metric_events PRIMARY KEY (id)
 );
